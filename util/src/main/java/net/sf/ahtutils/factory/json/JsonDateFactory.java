b324fee1b243faab2809983fc140387b645a0e19
Thorsten <t.kisner@web.de>
2016-01-25 08:00:18 +0100

    deprecated apply method removed

--- util/src/main/java/net/sf/ahtutils/factory/json/JsonDateFactory.java
+++ util/src/main/java/net/sf/ahtutils/factory/json/JsonDateFactory.java
@@ -1,8 +1,6 @@
 package net.sf.ahtutils.factory.json;
 
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
@@ -15,25 +13,10 @@ import net.sf.ahtutils.interfaces.model.date.EntityWithDay;
 import net.sf.ahtutils.interfaces.model.date.EntityWithMonth;
 import net.sf.ahtutils.interfaces.model.date.EntityWithQuarter;
 import net.sf.ahtutils.interfaces.model.date.EntityWithWeek;
-import net.sf.ahtutils.interfaces.model.date.EntityWithYearMonthQuarter;
 
 public class JsonDateFactory
 {
 	final static Logger logger = LoggerFactory.getLogger(AhtUtilsBootstrap.class);
-	
-	@Deprecated //use build method
-	public static void apply(EntityWithYearMonthQuarter json, Date date) 
-	{
-		DateTime dt = new DateTime(date);
-		Calendar cal = Calendar.getInstance(Locale.US);
-		cal.setTime(date);
-		int month = cal.get(Calendar.MONTH);
-		int quarter = (month / 3) + 1;
-		
-		json.setYear(dt.getYear());
-		json.setQuarter("Q" + quarter);
-		json.setMonth(dt.getMonthOfYear());
-	}
 	
 	public static void build(EntityWithDate json, Date date) 
 	{

a207dcbac66071c48dc82eed57337005cb485023
RebeccaRoblick <rebecca.roblick@gmail.com>
2016-02-02 12:42:45 +0100

    changed shortcuts for pdf and xls glossary entries Test for hash code implemented

--- util/src/main/java/net/sf/ahtutils/util/comparator/primitive/HashCodeComparator.java
+++ util/src/main/java/net/sf/ahtutils/util/comparator/primitive/HashCodeComparator.java
@@ -104,14 +104,5 @@ public class HashCodeComparator
 
 	public Map<String, String> getHash(){return hash;}
 
-	public static void main(String[] args)
-	{
-		File f = new File("..\\meis\\entities");
-		HashCodeComparator hcc = new HashCodeComparator(f);
-		hcc.searchForHashCodeBuilder();
-		Map<String, String> testList = hcc.compareCode();
-		for(Map.Entry<String, String> entry : testList.entrySet())
-			System.out.println(entry);
-		System.out.println(testList.size());
-	}
+
 }
--- util/src/test/java/net/sf/ahtutils/util/comparator/primitive/TestHashCodeComparator.java
+++ util/src/test/java/net/sf/ahtutils/util/comparator/primitive/TestHashCodeComparator.java
@@ -1,8 +1,19 @@
 package net.sf.ahtutils.util.comparator.primitive;
 
-/**
- * Created by roblick on 24.11.2015.
- */
+import java.io.File;
+import java.util.Map;
+
+
 public class TestHashCodeComparator
 {
+	public static void main(String[] args)
+	{
+		File f = new File("..\\meis\\entities");
+		HashCodeComparator hcc = new HashCodeComparator(f);
+		hcc.searchForHashCodeBuilder();
+		Map<String, String> testList = hcc.compareCode();
+		for(Map.Entry<String, String> entry : testList.entrySet())
+			System.out.println(entry);
+		System.out.println(testList.size());
+	}
 }

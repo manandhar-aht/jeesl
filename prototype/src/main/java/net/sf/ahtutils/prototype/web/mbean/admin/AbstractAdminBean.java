e3a640d28687ac9d979019d64b95a88e501d80a0
Thorsten <t.kisner@web.de>
2016-02-08 09:04:55 +0100

    merging abstract admin beans

--- prototype/src/main/java/net/sf/ahtutils/prototype/web/mbean/admin/AbstractAdminBean.java
+++ prototype/src/main/java/net/sf/ahtutils/prototype/web/mbean/admin/AbstractAdminBean.java
@@ -11,6 +11,7 @@ import net.sf.ahtutils.interfaces.bean.FacesMessageBean;
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.web.UtilsJsfSecurityHandler;
+import net.sf.exlp.util.io.StringUtil;
 
 public class AbstractAdminBean <L extends UtilsLang,D extends UtilsDescription>
 					implements Serializable
@@ -26,13 +27,24 @@ public class AbstractAdminBean <L extends UtilsLang,D extends UtilsDescription>
 	protected EjbLangFactory<L> efLang;
 	protected EjbDescriptionFactory<D> efDescription;
 	
+	protected boolean hasDeveloperAction;public boolean isHasDeveloperAction() {return hasDeveloperAction;}
+	protected boolean hasAdministratorAction;
+	protected boolean hasTranslatorAction;
+	
 	public AbstractAdminBean()
 	{
 		debugOnInfo = false;
 		
+		hasDeveloperAction = true;
+		hasAdministratorAction = true;
+		hasTranslatorAction = true;
+		
 		uiAllowAdd = true;
 		uiAllowSave = true;
+		uiAllowRemove = true;
 		uiAllowReorder = true;
+		uiAllowCode = true;
+		
 		uiShowInvisible = true;
 		
 	}
@@ -49,12 +61,52 @@ public class AbstractAdminBean <L extends UtilsLang,D extends UtilsDescription>
 	//Security Handling
 	protected boolean uiAllowAdd;public boolean isUiAllowAdd() {return uiAllowAdd;}
 	protected boolean uiAllowSave; public boolean getUiAllowSave() {return uiAllowSave;}
+	protected boolean uiAllowRemove; public boolean isUiAllowRemove() {return uiAllowRemove;}
 	protected boolean uiAllowReorder; public boolean getUiAllowReorder() {return uiAllowReorder;}
+	protected boolean uiAllowCode;public boolean isUiAllowCode() {return uiAllowCode;}
 	protected boolean uiShowInvisible; public boolean isUiShowInvisible() {return uiShowInvisible;}
 	
 	protected void updateSecurity2(UtilsJsfSecurityHandler jsfSecurityHandler, String viewCode)
 	{
+		String actionDeveloper = viewCode+".Developer";
+		String actionAdministrator = viewCode+".Administrator";
+		String actionTranslator = viewCode+".Translator";
 		
+		hasDeveloperAction = jsfSecurityHandler.allow(actionDeveloper);
+		hasAdministratorAction = jsfSecurityHandler.allow(actionAdministrator);
+		hasTranslatorAction = jsfSecurityHandler.allow(actionTranslator);
+		
+		uiAllowAdd = hasDeveloperAction || hasAdministratorAction;
+		uiAllowSave = hasDeveloperAction || hasAdministratorAction || hasTranslatorAction;
+		uiAllowRemove = hasDeveloperAction || hasAdministratorAction;
+		uiAllowReorder = hasDeveloperAction || hasAdministratorAction;
+		uiAllowCode = hasDeveloperAction;
+		uiShowInvisible = hasDeveloperAction;
 	}
 	
+	protected void debugUi(boolean debug)
+	{
+		if(debug)
+		{
+			logger.info(StringUtil.stars());
+			logger.info("UI Settings");
+			logger.info("\t"+uiAllowAdd+" allowStatusAdd");
+			logger.info("\t"+uiAllowReorder+" uiAllowReorder");
+			logger.info("\t"+uiAllowCode+" uiAllowCode");
+			logger.info("\t"+uiAllowSave+" uiAllowSave");
+			logger.info("\t"+uiAllowRemove+" uiAllowRemove");
+		}
+	}
+	
+	protected void debugSecurity(boolean debug)
+	{
+		if(debug)
+		{
+			logger.info(StringUtil.stars());
+			logger.info("Security Settings");
+			logger.info("\t"+hasDeveloperAction+" hasDeveloperAction");
+			logger.info("\t"+hasAdministratorAction+" hasAdministratorAction");
+			logger.info("\t"+hasTranslatorAction+" hasTranslatorAction");
+		}
+	}
 }
--- prototype/src/main/java/net/sf/ahtutils/prototype/web/mbean/admin/utils/AbstractOptionTableBean.java
+++ prototype/src/main/java/net/sf/ahtutils/prototype/web/mbean/admin/utils/AbstractOptionTableBean.java
@@ -20,7 +20,6 @@ import net.sf.ahtutils.interfaces.model.status.UtilsWithImage;
 import net.sf.ahtutils.interfaces.model.status.UtilsWithSymbol;
 import net.sf.ahtutils.interfaces.web.UtilsJsfSecurityHandler;
 import net.sf.ahtutils.prototype.web.mbean.admin.AbstractAdminBean;
-import net.sf.exlp.util.io.StringUtil;
 
 public class AbstractOptionTableBean <L extends UtilsLang,
 										D extends UtilsDescription,
@@ -33,17 +32,12 @@ public class AbstractOptionTableBean <L extends UtilsLang,
 	final static Logger logger = LoggerFactory.getLogger(AbstractOptionTableBean.class);
 	private static final long serialVersionUID = 1L;
 
-	protected boolean allowCodeChange;public boolean isAllowCodeChange() {return allowCodeChange;}
-	protected boolean allowRemove; public boolean isAllowRemove() {return allowRemove;}
 	protected boolean allowSvg; public boolean isAllowSvg() {return allowSvg;}
 	
 	protected boolean supportsSymbol; public boolean getSupportsSymbol(){return supportsSymbol;}
 	protected boolean supportsImage; public boolean getSupportsImage() {return supportsImage;}
 	protected boolean supportsGraphic; public boolean getSupportsGraphic() {return supportsGraphic;}
-	
-	protected boolean hasDeveloperAction;public boolean isHasDeveloperAction() {return hasDeveloperAction;}
-	protected boolean hasAdministratorAction,hasTranslatorAction;
-	
+		
 	protected long index;
 	protected Map<Long,Boolean> allowAdditionalElements; public Map<Long, Boolean> getAllowAdditionalElements(){return allowAdditionalElements;}
 	
@@ -61,8 +55,6 @@ public class AbstractOptionTableBean <L extends UtilsLang,
 		hasDeveloperAction = false;
 		hasAdministratorAction = true;
 		hasTranslatorAction = true;
-
-		allowRemove = true;
 		
 		allowAdditionalElements = new Hashtable<Long,Boolean>();
 	}
@@ -75,17 +67,7 @@ public class AbstractOptionTableBean <L extends UtilsLang,
 	
 	protected void updateSecurity(UtilsJsfSecurityHandler jsfSecurityHandler, String viewCode)
 	{
-		String actionDeveloper = viewCode+".Developer";
-		String actionAdministrator = viewCode+".Administrator";
-		String actionTranslator = viewCode+".Translator";
-		
-		hasDeveloperAction = jsfSecurityHandler.allow(actionDeveloper);
-		hasAdministratorAction = jsfSecurityHandler.allow(actionAdministrator);
-		hasTranslatorAction = jsfSecurityHandler.allow(actionTranslator);
-		
-		uiAllowReorder = hasDeveloperAction || hasAdministratorAction;
-		uiAllowSave = hasDeveloperAction || hasAdministratorAction || hasTranslatorAction;
-		allowRemove = hasDeveloperAction || hasAdministratorAction;
+		super.updateSecurity2(jsfSecurityHandler, viewCode);
 	}
 	
 	protected void updateUiForCategory()
@@ -103,30 +85,4 @@ public class AbstractOptionTableBean <L extends UtilsLang,
 	}
 	
 	public void reorder() throws UtilsConstraintViolationException, UtilsLockingException {}
-	
-	public void debugSecurity(boolean debug)
-	{
-		if(debug)
-		{
-			logger.info(StringUtil.stars());
-			logger.info("Security");
-			logger.info("\t"+hasDeveloperAction+" hasDeveloperAction");
-			logger.info("\t"+hasAdministratorAction+" hasAdministratorAction");
-			logger.info("\t"+hasTranslatorAction+" hasTranslatorAction");
-		}
-	}
-	
-	public void debugUi(boolean debug)
-	{
-		if(debug)
-		{
-			logger.info(StringUtil.stars());
-			logger.info("Page Actions");
-			logger.info("\t"+uiAllowAdd+" allowStatusAdd");
-			logger.info("\t"+uiAllowReorder+" uiAllowReorder");
-			logger.info("\t"+allowCodeChange+" allowCodeChange");
-			logger.info("\t"+uiAllowSave+" uiAllowSave");
-			logger.info("\t"+allowRemove+" allowRemove");
-		}
-	}
 }

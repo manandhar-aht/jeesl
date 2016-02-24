c1f7a968d9dfc4beeedfcf957d6b57acaa286b9c
RebeccaRoblick <rebecca.roblick@gmail.com>
2016-02-24 16:26:49 +0100

    manual additions

--- doc/src/main/resources/listing.aht-utils/admin/db/postgres/template/debian/wheezy.sql
+++ doc/src/main/resources/listing.aht-utils/admin/db/postgres/template/debian/wheezy.sql
@@ -1,6 +1,6 @@
 createdb template_postgis
 createlang plpgsql template_postgis
 psql -d template_postgis -c "UPDATE pg_database SET datistemplate=true WHERE datname='template_postgis'"
-psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
-psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
-psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql
+psql -d template_postgis -f /usr/share/postgresql/9.4/contrib/postgis-2.1/postgis.sql
+psql -d template_postgis -f /usr/share/postgresql/9.4/contrib/postgis-2.1/spatial_ref_sys.sql
+psql -d template_postgis -f /usr/share/postgresql/9.4/contrib/postgis_comments.sql

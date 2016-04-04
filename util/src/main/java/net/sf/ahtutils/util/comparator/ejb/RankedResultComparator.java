664e6e2bbae93ca36a70793d2eee2b36ca1cbe20
Thorsten <t.kisner@web.de>
2016-04-04 08:46:35 +0200

    RankedResultComparator added

---
+++ util/src/main/java/net/sf/ahtutils/util/comparator/ejb/RankedResultComparator.java
@@ -0,0 +1,22 @@
+package net.sf.ahtutils.util.comparator.ejb;
+
+import java.util.Comparator;
+
+import org.apache.commons.lang.builder.CompareToBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.sf.ahtutils.interfaces.model.ranking.UtilsRankedResult;
+import net.sf.ahtutils.model.interfaces.with.EjbWithId;
+
+public class RankedResultComparator<RR extends UtilsRankedResult<T>, T extends EjbWithId> implements Comparator<RR>
+{
+	final static Logger logger = LoggerFactory.getLogger(RankedResultComparator.class);
+
+	public int compare(RR a, RR b)
+    {
+		  CompareToBuilder ctb = new CompareToBuilder();
+		  ctb.append(a.getRanking(), b.getRanking());
+		  return ctb.toComparison();
+    }
+}

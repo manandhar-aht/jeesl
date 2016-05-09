ba2a675774703632dd57a4143d15ddeae2c6a560
Thorsten <t.kisner@web.de>
2016-05-09 08:59:06 +0200

    DynamicPivotAggregator added

---
+++ report/src/main/java/net/sf/ahtutils/report/analysis/DynamicPivotData.java
@@ -0,0 +1,42 @@
+package net.sf.ahtutils.report.analysis;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.sf.ahtutils.model.interfaces.with.EjbWithId;
+
+public class DynamicPivotData
+{
+    final static Logger logger = LoggerFactory.getLogger(DynamicPivotData.class);
+
+    private int size;
+    public int getSize() {
+		return size;
+	}
+
+	private double value;public double getValue() {return value;}
+    
+    private List<EjbWithId> entities;  public List<EjbWithId> getEntities() {return entities;}
+
+	public DynamicPivotData(int size)
+    {
+		this.size=size;
+    	entities = new ArrayList<EjbWithId>();
+    }
+    
+    public void setData(double value, EjbWithId... entities)
+    {
+    	this.value=value;
+    	this.entities.addAll(new ArrayList<EjbWithId>(Arrays.asList(entities)));
+    }
+    
+    public EjbWithId getEntity(int index)
+    {
+    	return entities.get(index);
+    }
+    
+}
--- report/src/main/java/net/sf/ahtutils/report/analysis/SimplePivotData.java
+++ report/src/main/java/net/sf/ahtutils/report/analysis/SimplePivotData.java
@@ -9,39 +9,23 @@ public class SimplePivotData <T extends EjbWithId, P extends EjbWithId>
 {
     final static Logger logger = LoggerFactory.getLogger(SimplePivotData.class);
 
+    private double value;
     private T t;
     private P p;
-    private double value;
     
-    
-    public T getT() {
-		return t;
-	}
-
-	public void setT(T t) {
-		this.t = t;
-	}
-
-	public P getP() {
-		return p;
-	}
-
-	public void setP(P p) {
-		this.p = p;
-	}
-
-	public double getValue() {
-		return value;
-	}
-
-	public void setValue(double value) {
-		this.value = value;
-	}
-
 	public SimplePivotData(T t, P p, double value)
     {
     	this.t=t;
     	this.p=p;
     	this.value=value;
     }
+    
+    public T getT() {return t;}
+	public void setT(T t) {this.t = t;}
+
+	public P getP() {return p;}
+	public void setP(P p) {this.p = p;}
+
+	public double getValue() {return value;}
+	public void setValue(double value) {this.value = value;}
 }

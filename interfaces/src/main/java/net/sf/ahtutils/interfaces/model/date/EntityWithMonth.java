4513dd03503beb620567a93838c9756e1413c118
Thorsten <t.kisner@web.de>
2016-05-31 08:41:04 +0200

    EntityWithMonth improved

--- interfaces/src/main/java/net/sf/ahtutils/interfaces/model/date/EntityWithMonth.java
+++ interfaces/src/main/java/net/sf/ahtutils/interfaces/model/date/EntityWithMonth.java
@@ -1,6 +1,6 @@
 package net.sf.ahtutils.interfaces.model.date;
 
-public interface EntityWithMonth
+public interface EntityWithMonth extends EntityWithDate
 {
     public int getMonth();
     public void setMonth(int month);

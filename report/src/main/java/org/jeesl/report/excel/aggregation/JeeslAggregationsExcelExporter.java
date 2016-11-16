b634eb09e1cc19f053066d073cec26f16323f0c2
Thorsten <t.kisner@web.de>
2016-11-16 15:00:01 +0100

    JeeslAggregationsExcelExporter added

---
+++ report/src/main/java/org/jeesl/report/excel/aggregation/JeeslAggregationsExcelExporter.java
@@ -0,0 +1,44 @@
+package org.jeesl.report.excel.aggregation;
+
+import java.io.InputStream;
+
+import org.jeesl.interfaces.model.system.io.report.JeeslReportGrouping;
+
+import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
+import net.sf.ahtutils.interfaces.model.status.UtilsLang;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
+import net.sf.ahtutils.xml.finance.Figures;
+import net.sf.ahtutils.xml.report.Info;
+
+public class JeeslAggregationsExcelExporter
+{
+    public static <L extends UtilsLang,D extends UtilsDescription,TYPE extends UtilsStatus<TYPE,L,D>>
+    
+    InputStream export(TYPE type, Info info, Figures figures)
+    {
+    	switch(JeeslReportGrouping.Code.valueOf(type.getCode()))
+    	{
+    		case flat: return flat(info,figures);
+    		case hierarchical: return hierarchical(info,figures);
+    		default: return null;
+    	}
+    }
+    
+    private static InputStream flat(Info info, Figures figures)
+    {
+/*    	ExcelAggregationsExporter exporter = new ExcelAggregationsExporter(r);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		exporter.getWb().write(baos);
+		return new ByteArrayInputStream(baos.toByteArray());
+*/ 		return null; 
+    }
+    
+    private static InputStream hierarchical(Info info, Figures figures)
+    {
+/*    	ExcelAggregationsExporter exporter = new ExcelAggregationsExporter(r);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		exporter.getWb().write(baos);
+		return new ByteArrayInputStream(baos.toByteArray());
+*/ 		return null; 
+    }
+}

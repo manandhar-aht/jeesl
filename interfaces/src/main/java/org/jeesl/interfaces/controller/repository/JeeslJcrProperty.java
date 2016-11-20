f58ea4ec05e6e3b9f07ea8d10897a337d2ce1de4
Thorsten <t.kisner@web.de>
2016-11-20 15:25:12 +0100

    JeeslJcrProperties added

---
+++ interfaces/src/main/java/org/jeesl/interfaces/controller/repository/JeeslJcrProperty.java
@@ -0,0 +1,16 @@
+package org.jeesl.interfaces.controller.repository;
+
+public interface JeeslJcrProperty
+{
+	public static String jcrContent = "jcr:content";
+	public static String jcrData = "jcr:data";
+	
+	public static String ntFile = "nt:file";
+	public static String ntResource = "nt:resource";
+	
+	public static String fileName = "fileName";
+	public static String fileDate = "fileDate";
+
+	public static String fileType = "fileType";
+	public enum FileTypes {doc,xls,pdf};
+}

4a06dda78a46013f4a1970e7171c211c92e7e8e1
Helge Hemmer <hhemmer@web.de>
2016-11-25 11:09:02 +0100

    Added modified flat and hierachical Excel exporters to be used in general Jeesl Excel aggregations exporter

--- report/src/main/java/org/jeesl/report/excel/aggregation/JeeslAggregationsExcelExporter.java
+++ report/src/main/java/org/jeesl/report/excel/aggregation/JeeslAggregationsExcelExporter.java
@@ -1,5 +1,8 @@
 package org.jeesl.report.excel.aggregation;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.jeesl.interfaces.model.system.io.report.JeeslReportGrouping;
@@ -13,36 +16,35 @@ import net.sf.ahtutils.xml.report.Info;
 public class JeeslAggregationsExcelExporter
 {
     public static <L extends UtilsLang,D extends UtilsDescription,TYPE extends UtilsStatus<TYPE,L,D>>
-    	InputStream export(TYPE type, Info info, Figures figures)
+    	InputStream export(TYPE type, Info info, Figures figures) throws IOException
     {
     	switch(JeeslReportGrouping.Code.valueOf(type.getCode()))
     	{
-    		case flat: return flat(info,figures);
-    		case hierarchical: return hierarchical(info,figures);
-    		default: return null;
+    		case flat:			return flat(info,figures);
+    		case hierarchical:	return hierarchical(info,figures);
+    		default:			return null;
     	}
     }
     
-    public static InputStream export(Info info, Figures figures)
+    public static InputStream export(Info info, Figures figures) throws IOException
 	{
 		return hierarchical(info,figures);
 	}
     
-    private static InputStream flat(Info info, Figures figures)
+    private static InputStream flat(Info info, Figures figures) throws IOException
     {
-/*    	ExcelAggregationsExporter exporter = new ExcelAggregationsExporter(r);
+    	JeeslAggregationsFlatExporter exporter = new JeeslAggregationsFlatExporter(info, figures);
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		exporter.getWb().write(baos);
 		return new ByteArrayInputStream(baos.toByteArray());
-*/ 		return null; 
+		
     }
     
-    private static InputStream hierarchical(Info info, Figures figures)
+    private static InputStream hierarchical(Info info, Figures figures) throws IOException
     {
-/*    	ExcelAggregationsExporter exporter = new ExcelAggregationsExporter(r);
+    	JeeslAggregationsHierachicalExporter exporter = new JeeslAggregationsHierachicalExporter(info, figures);
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		exporter.getWb().write(baos);
 		return new ByteArrayInputStream(baos.toByteArray());
-*/ 		return null; 
     }
 }

c47107b6189e8ecf00478ddf335551d39aee9057
Thorsten <t.kisner@web.de>
2016-11-30 07:50:36 +0200

    JcrProperties improved

--- interfaces/src/main/java/org/jeesl/interfaces/controller/repository/JeeslJcrProperty.java
+++ interfaces/src/main/java/org/jeesl/interfaces/controller/repository/JeeslJcrProperty.java
@@ -4,9 +4,12 @@ public interface JeeslJcrProperty
 {
 	public static String jcrContent = "jcr:content";
 	public static String jcrData = "jcr:data";
+	public static String jcrEncoding = "jcr:encoding";
 	
 	public static String ntFile = "nt:file";
 	public static String ntResource = "nt:resource";
+	
+	public static String jeeslFileCategory = "jeesl:category";
 	
 	public static String fileName = "fileName";
 	public static String fileDate = "fileDate";

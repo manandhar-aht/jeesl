1dce101592e3f9bf5b7c5a2fb48209ac654abedd
Thorsten <t.kisner@web.de>
2016-12-02 08:01:48 +0200

    TxtSurveyQuestionFactory added

--- util/src/main/java/org/jeesl/factory/txt/survey/TxtSurveyQuestionFactory.java
+++ util/src/main/java/org/jeesl/factory/txt/survey/TxtSurveyQuestionFactory.java
@@ -1,8 +1,11 @@
-package org.jeesl.factory.ejb.survey;
+package org.jeesl.factory.txt.survey;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.jeesl.interfaces.model.survey.JeeslSurvey;
 import org.jeesl.interfaces.model.survey.JeeslSurveyAnswer;
@@ -19,9 +22,8 @@ import org.slf4j.LoggerFactory;
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
-import net.sf.ahtutils.xml.survey.Answer;
 
-public class EjbSurveyAnswerFactory<L extends UtilsLang,
+public class TxtSurveyQuestionFactory <L extends UtilsLang,
 										D extends UtilsDescription,
 										SURVEY extends JeeslSurvey<L,D,SURVEY,SS,TEMPLATE,VERSION,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,
 										SS extends UtilsStatus<SS,L,D>,
@@ -37,56 +39,44 @@ public class EjbSurveyAnswerFactory<L extends UtilsLang,
 										OPTION extends JeeslSurveyOption<L,D,SURVEY,SS,TEMPLATE,VERSION,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,
 										CORRELATION extends JeeslSurveyCorrelation<L,D,SURVEY,SS,TEMPLATE,VERSION,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>>
 {
-	final static Logger logger = LoggerFactory.getLogger(EjbSurveyAnswerFactory.class);
+	final static Logger logger = LoggerFactory.getLogger(TxtSurveyQuestionFactory.class);
+		
+	private DecimalFormat df = new DecimalFormat("0.#");
 	
-	final Class<ANSWER> cAnswer;
-    
-	public EjbSurveyAnswerFactory(final Class<ANSWER> cAnswer)
-	{       
-        this.cAnswer = cAnswer;
-	}
-   	
-	public ANSWER build(QUESTION question, DATA data,Answer answer)
+	public Map<QUESTION,String> scoreBoundsAnswers(List<ANSWER> list)
 	{
-		Boolean valueBoolean = null;if(answer.isSetValueBoolean()){valueBoolean=answer.isValueBoolean();}
-		Integer valueNumber = null;if(answer.isSetValueNumber()){valueNumber=answer.getValueNumber();}
-		return build(question, data,valueBoolean,valueNumber);
-	}
-	public ANSWER build(QUESTION question, DATA data)
-	{
-		return build(question, data,null,null);
-	}
-	
-	
-	public ANSWER build(QUESTION question, DATA data,Boolean valueBoolean,Integer valueNumber)
-	{
-		ANSWER ejb = null;
-		try
+		List<QUESTION> questions = new ArrayList<QUESTION>();
+		for(ANSWER a : list)
 		{
-			ejb = cAnswer.newInstance();
-			ejb.setQuestion(question);
-			ejb.setData(data);
-			ejb.setValueBoolean(valueBoolean);
-			ejb.setValueNumber(valueNumber);
+			questions.add(a.getQuestion());
 		}
-		catch (InstantiationException e) {e.printStackTrace();}
-		catch (IllegalAccessException e) {e.printStackTrace();}
-		
-		return ejb;
+		return scoreBoundsQuestions(questions);
 	}
 	
-	public List<ANSWER> toSectionAnswers(SECTION section, Map<QUESTION,ANSWER> map)
+	public Map<QUESTION,String> scoreBoundsQuestions(Set<QUESTION> set)
 	{
-		List<ANSWER> list = new ArrayList<ANSWER>();
-		
-		for(QUESTION q : map.keySet())
+		List<QUESTION> questions = new ArrayList<QUESTION>(set);
+		return scoreBoundsQuestions(questions);
+	}
+	
+	public Map<QUESTION,String> scoreBoundsQuestions(List<QUESTION> list)
+	{
+		Map<QUESTION,String> map = new HashMap<QUESTION,String>();
+		for(QUESTION q : list)
 		{
-			if(q.getSection().equals(section))
-			{
-				list.add(map.get(q));
-			}
+			map.put(q,scoreBounds(q));
 		}
-		
-		return list;
+		return map;
+	}
+	
+	public String scoreBounds(QUESTION q)
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append("[");
+		sb.append(df.format(q.getMinScore()));
+		sb.append(",");
+		sb.append(df.format(q.getMaxScore()));
+		sb.append("]");
+		return sb.toString();
 	}
 }

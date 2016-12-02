21dc402fc83b79eac3453c4711d87c8f1f69b032
Thorsten <t.kisner@web.de>
2016-12-02 21:55:16 +0200

    LisGraphicFacade added

--- ejb/src/main/java/org/jeesl/controller/facade/JeeslGraphicFacadeBean.java
+++ ejb/src/main/java/org/jeesl/controller/facade/JeeslGraphicFacadeBean.java
@@ -1,14 +1,22 @@
 package org.jeesl.controller.facade;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
 
 import org.jeesl.interfaces.facade.JeeslGraphicFacade;
 
 import net.sf.ahtutils.controller.facade.UtilsFacadeBean;
+import net.sf.ahtutils.exception.ejb.UtilsNotFoundException;
 import net.sf.ahtutils.interfaces.model.graphic.UtilsGraphic;
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
+import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 
 public class JeeslGraphicFacadeBean<L extends UtilsLang,
 									D extends UtilsDescription,
@@ -24,5 +32,24 @@ public class JeeslGraphicFacadeBean<L extends UtilsLang,
 	{
 		super(em);
 		this.cG=cG;
+	}
+
+	@Override
+	public <T extends EjbWithId> G fGraphic(Class<T> c, long statusId) throws UtilsNotFoundException
+	{
+		CriteriaBuilder cB = em.getCriteriaBuilder();
+		
+		CriteriaQuery<G> cQ = cB.createQuery(cG);
+		Root<T> monitoring = cQ.from(c);
+		
+		Path<G> pathProject = monitoring.get("graphic");
+		Path<Long> pId = monitoring.get("id");
+		
+		cQ.where(cB.equal(pId,statusId));
+		cQ.select(pathProject);
+		
+		try	{return em.createQuery(cQ).getSingleResult();}
+		catch (NoResultException ex){throw new UtilsNotFoundException("No Graphic found for status.id"+statusId);}
+		catch (NonUniqueResultException ex){throw new UtilsNotFoundException("Multiple Results for status.id"+statusId);}
 	}
 }
--- interfaces/src/main/java/org/jeesl/interfaces/facade/JeeslGraphicFacade.java
+++ interfaces/src/main/java/org/jeesl/interfaces/facade/JeeslGraphicFacade.java
@@ -1,10 +1,12 @@
 package org.jeesl.interfaces.facade;
 
+import net.sf.ahtutils.exception.ejb.UtilsNotFoundException;
 import net.sf.ahtutils.interfaces.facade.UtilsFacade;
 import net.sf.ahtutils.interfaces.model.graphic.UtilsGraphic;
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
+import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 
 public interface JeeslGraphicFacade <L extends UtilsLang,
 									D extends UtilsDescription,
@@ -13,5 +15,5 @@ public interface JeeslGraphicFacade <L extends UtilsLang,
 									GS extends UtilsStatus<GS,L,D>>
 			extends UtilsFacade
 {	
-
+	<T extends EjbWithId> G fGraphic(Class<T> c, long statusId) throws UtilsNotFoundException;
 }

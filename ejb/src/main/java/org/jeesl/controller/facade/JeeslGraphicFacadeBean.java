c9c80e66d8e0ea0b65eba6196108f5d58470f7c4
Thorsten <t.kisner@web.de>
2016-12-02 21:11:43 +0200

    GraphicsFacade renamed

--- ejb/src/main/java/org/jeesl/controller/facade/JeeslGraphicFacadeBean.java
+++ ejb/src/main/java/org/jeesl/controller/facade/JeeslGraphicFacadeBean.java
@@ -1,23 +1,28 @@
-package net.sf.ahtutils.controller.facade;
+package org.jeesl.controller.facade;
 
 import javax.persistence.EntityManager;
 
-import net.sf.ahtutils.interfaces.facade.UtilsGraphicFacade;
+import org.jeesl.interfaces.facade.JeeslGraphicFacade;
+
+import net.sf.ahtutils.controller.facade.UtilsFacadeBean;
 import net.sf.ahtutils.interfaces.model.graphic.UtilsGraphic;
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
 
-public class UtilsGraphicFacadeBean<L extends UtilsLang,
+public class JeeslGraphicFacadeBean<L extends UtilsLang,
 									D extends UtilsDescription,
 									G extends UtilsGraphic<L,D,G,GT,GS>,
 									GT extends UtilsStatus<GT,L,D>,
 									GS extends UtilsStatus<GS,L,D>>
 					extends UtilsFacadeBean
-					implements UtilsGraphicFacade<L,D,G,GT,GS>
+					implements JeeslGraphicFacade<L,D,G,GT,GS>
 {	
-	public UtilsGraphicFacadeBean(EntityManager em)
+	private final Class<G> cG;
+	
+	public JeeslGraphicFacadeBean(EntityManager em, final Class<G> cG)
 	{
 		super(em);
+		this.cG=cG;
 	}
 }
--- interfaces/src/main/java/org/jeesl/interfaces/facade/JeeslGraphicFacade.java
+++ interfaces/src/main/java/org/jeesl/interfaces/facade/JeeslGraphicFacade.java
@@ -1,11 +1,12 @@
-package net.sf.ahtutils.interfaces.facade;
+package org.jeesl.interfaces.facade;
 
+import net.sf.ahtutils.interfaces.facade.UtilsFacade;
 import net.sf.ahtutils.interfaces.model.graphic.UtilsGraphic;
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
 
-public interface UtilsGraphicFacade <L extends UtilsLang,
+public interface JeeslGraphicFacade <L extends UtilsLang,
 									D extends UtilsDescription,
 									G extends UtilsGraphic<L,D,G,GT,GS>,
 									GT extends UtilsStatus<GT,L,D>,

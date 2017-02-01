42af8e687e1e8819c335cd24a31685790a13268a
Thorsten <t.kisner@web.de>
2017-02-01 09:25:33 +0100

    HumanFileSize component added

---
+++ jsf/src/main/java/org/jeesl/jsf/functions/HumanFileSize.java
@@ -0,0 +1,23 @@
+package org.jeesl.jsf.functions;
+
+public final class HumanFileSize
+{
+	public HumanFileSize()
+    {
+
+    }
+    
+    public static String humanFileSize(Long value)
+    {
+    	return humanReadableByteCount(value,true);
+    }
+    
+    public static String humanReadableByteCount(long bytes, boolean si)
+    {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
+}

b4de32d129ecfc8a24d3fe707b92c3f6e38d5508
Thorsten <t.kisner@web.de>
2017-02-05 12:50:51 +0200

    NullNumberBinder now handles Doubles

--- jsf/src/main/java/org/jeesl/jsf/util/NullNumberBinder.java
+++ jsf/src/main/java/org/jeesl/jsf/util/NullNumberBinder.java
@@ -1,4 +1,4 @@
-package net.sf.ahtutils.jsf.util;
+package org.jeesl.jsf.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,13 +17,17 @@ public class NullNumberBinder
 	public String getA() {return a;}
 	public void setA(String a) {this.a = a;}
 	public void integerToA(Integer i) {a = toString(i);}
-	public Integer aToInteger() {return fromString(a);}
+	public void doubleToA(Double d) {a = toString(d);}
+	public Integer aToInteger() {return integerFromString(a);}
+	public Double aToDouble() {return doubleFromString(a);}
 	
 	private String b;
 	public String getB() {return b;}
 	public void setB(String b) {this.b = b;}
 	public void integerToB(Integer i) {b = toString(i);}
-	public Integer bToInteger() {return fromString(b);}
+	public void doubleToB(Double d) {b = toString(d);}
+	public Integer bToInteger() {return integerFromString(b);}
+	public Double bToDouble() {return doubleFromString(b);}
 	
 	private String toString(Integer i)
 	{
@@ -44,13 +48,32 @@ public class NullNumberBinder
 		return sb.toString();
 	}
 	
-	private Integer fromString(String x)
+	private String toString(Double d)
+	{
+		StringBuffer sb = new StringBuffer();
+		if(d==null){sb.append("");}
+		else{sb.append(d.doubleValue());}
+		
+		if(logger.isTraceEnabled())
+		{
+			StringBuffer sbd = new StringBuffer();
+			sbd.append("Double ");
+			if(d==null){sbd.append("null");}
+			else{sbd.append(d.doubleValue());}
+			sbd.append(" returns ").append(sb.toString());
+			logger.trace(sbd.toString());
+		}
+		
+		return sb.toString();
+	}
+	
+	private Integer integerFromString(String x)
 	{
 		Integer result = null;
 		if(x==null || x.trim().length()==0){}
 		else
 		{
-			result = new Integer(x);
+			result = Integer.valueOf(x);
 		}
 		
 		if(logger.isTraceEnabled())
@@ -60,6 +83,28 @@ public class NullNumberBinder
 			sb.append(" returns ");
 			if(result==null){sb.append("null");}
 			else{sb.append(result.intValue());}
+			logger.trace(sb.toString());
+		}
+		
+		return result;
+	}
+	
+	private Double doubleFromString(String x)
+	{
+		Double result = null;
+		if(x==null || x.trim().length()==0){}
+		else
+		{
+			result = Double.valueOf(x);
+		}
+		
+		if(logger.isTraceEnabled())
+		{
+			StringBuffer sb = new StringBuffer();
+			sb.append("String: ").append(x);
+			sb.append(" returns ");
+			if(result==null){sb.append("null");}
+			else{sb.append(result.doubleValue());}
 			logger.trace(sb.toString());
 		}
 		
---
+++ prototype/src/main/java/org/jeesl/web/mbean/prototype/system/AbstractLabelBean.java
@@ -0,0 +1,86 @@
+package org.jeesl.web.mbean.prototype.system;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.jeesl.interfaces.facade.JeeslRevisionFacade;
+import org.jeesl.interfaces.model.system.revision.UtilsRevisionAttribute;
+import org.jeesl.interfaces.model.system.revision.UtilsRevisionEntity;
+import org.jeesl.interfaces.model.system.revision.UtilsRevisionEntityMapping;
+import org.jeesl.interfaces.model.system.revision.UtilsRevisionScope;
+import org.jeesl.interfaces.model.system.revision.UtilsRevisionView;
+import org.jeesl.interfaces.model.system.revision.UtilsRevisionViewMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
+import net.sf.ahtutils.interfaces.model.status.UtilsLang;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
+
+public class AbstractLabelBean <L extends UtilsLang,D extends UtilsDescription,
+								RC extends UtilsStatus<RC,L,D>,
+								RV extends UtilsRevisionView<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RAT>,
+								RVM extends UtilsRevisionViewMapping<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RAT>,
+								RS extends UtilsRevisionScope<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RAT>,
+								RST extends UtilsStatus<RST,L,D>,
+								RE extends UtilsRevisionEntity<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RAT>,
+								REM extends UtilsRevisionEntityMapping<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RAT>,
+								RA extends UtilsRevisionAttribute<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RAT>,
+								RAT extends UtilsStatus<RAT,L,D>>
+					implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	final static Logger logger = LoggerFactory.getLogger(AbstractLabelBean.class);
+	
+	private JeeslRevisionFacade<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RAT> fRevision;
+	
+	private Class<RE> cRE;
+	
+	protected Map<String,Map<String,L>> entities; public Map<String,Map<String,L>> getEntities() {return entities;}
+	protected Map<String,Map<String,Map<String,L>>> labels; public Map<String, Map<String, Map<String,L>>> getLabels() {return labels;}
+	protected Map<String,Map<String,Map<String,D>>> descriptions;public Map<String, Map<String, Map<String,D>>> getDescriptions() {return descriptions;}
+	
+	
+	
+	protected void init(JeeslRevisionFacade<L,D,RC,RV,RVM,RS,RST,RE,REM,RA,RAT> fRevision, final Class<RE> cRE)
+	{
+		this.fRevision=fRevision;
+		this.cRE=cRE;
+		
+		entities = new HashMap<String,Map<String,L>>();
+		labels = new Hashtable<String,Map<String,Map<String,L>>>();
+		descriptions = new Hashtable<String,Map<String,Map<String,D>>>();
+		
+		for(RE re : fRevision.all(cRE))
+		{
+			reload(re);
+		}
+	}
+	
+	private void reload(RE re)
+	{
+		try
+		{
+			Class<?> c = Class.forName(re.getCode());
+			
+			re = fRevision.load(cRE, re);
+			if(entities.containsKey(c.getSimpleName())){logger.warn("Duplicate classs in Revisions "+re.getCode());}
+			
+			entities.put(c.getSimpleName(), re.getName());
+			labels.put(c.getSimpleName(), new Hashtable<String,Map<String,L>>());
+			descriptions.put(c.getSimpleName(), new Hashtable<String,Map<String,D>>());
+			
+			for(RA attribute : re.getAttributes())
+			{
+				if(attribute.getCode()!=null && attribute.getCode().trim().length()>0)
+				{
+					labels.get(c.getSimpleName()).put(attribute.getCode(), attribute.getName());
+					descriptions.get(c.getSimpleName()).put(attribute.getCode(), attribute.getDescription());
+				}
+			}
+		}
+		catch (ClassNotFoundException e) {logger.warn("CNFE: "+re.getCode());}
+	}
+}

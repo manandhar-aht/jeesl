203cc9ecc32763e12db19f407db627caf912ed76
Thorsten <t.kisner@web.de>
2017-02-07 17:00:32 +0200

    AbstractEjbQuery added

---
+++ interfaces/src/main/java/org/jeesl/model/ejb/AbstractEjbQuery.java
@@ -0,0 +1,64 @@
+package org.jeesl.model.ejb;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractEjbQuery implements Serializable
+{
+	final static Logger logger = LoggerFactory.getLogger(AbstractEjbQuery.class);
+	private static final long serialVersionUID = 1;
+
+	private String localeCode;public String getLocaleCode() {return localeCode;}
+
+	public AbstractEjbQuery() {this("en");}
+	public AbstractEjbQuery(String localeCode)
+	{
+		this.localeCode=localeCode;
+		distinct = false;
+	}
+	
+	private boolean distinct;
+	public boolean isDistinct() {return distinct;}
+	public void setDistinct(boolean distinct) {this.distinct = distinct;}
+
+	private Integer firstResult;
+	public Integer getFirstResult() {return firstResult;}
+	public void setFirstResult(Integer firstResult) {this.firstResult = firstResult;}
+
+	private Integer maxResults;
+	public Integer getMaxResults() {return maxResults;}
+	public void setMaxResults(Integer maxResults) {this.maxResults = maxResults;}
+	
+	//Sorting
+	private String sortBy;
+	public String getSortBy() {return sortBy;}
+
+	private boolean sortAscending;
+	public boolean isSortAscending() {return sortAscending;}
+	
+	public void sort(String sortBy, boolean sortAscending)
+	{
+		this.sortBy=sortBy;
+		this.sortAscending=sortAscending;
+	}
+	public void noSort()
+	{
+		sortBy=null;
+	}
+	public boolean withSort(){return (sortBy!=null && sortBy.trim().length()>0);}
+
+
+	protected void debug(boolean debug){debug(debug,0);}
+	protected void debug(boolean debug,int ident)
+	{
+		if(debug)
+		{
+			logger.info(StringUtils.repeat("\t",ident)+"distinct: "+distinct);
+			if(firstResult!=null){logger.info(StringUtils.repeat("\t",ident)+"firstResult: "+firstResult);}
+			if(maxResults!=null){logger.info(StringUtils.repeat("\t",ident)+"maxResults: "+maxResults);}
+		}
+	}
+}

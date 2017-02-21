cb8248b11b67017cedfc2b091d0cd1931197e8d1
Thorsten <t.kisner@web.de>
2017-02-21 09:26:15 +0100

    amount calculator improved

--- util/src/main/java/net/sf/ahtutils/controller/processor/finance/AmountCalculator.java
+++ util/src/main/java/net/sf/ahtutils/controller/processor/finance/AmountCalculator.java
@@ -2,6 +2,7 @@ package net.sf.ahtutils.controller.processor.finance;
 
 import java.util.List;
 
+import org.jeesl.controller.processor.finance.AmountRounder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,5 +33,30 @@ public class AmountCalculator
 		return sum;
 	}
 	
+	public static Double sum(Double...list)
+	{
+		Double sum = null;
+		for(Double value :list)
+		{
+			if(value!=null)
+			{
+				if(sum==null){sum = value;}
+				else {sum = sum + value;}
+			}
+		}
+		if(sum!=null){sum = AmountRounder.two(sum);}
+		return sum;
+	}
 	
+	public static Double minus(Double a, Double b)
+	{
+		Double result = null;
+		if(a!=null){result = a;}
+		else{result=Double.valueOf(0);}
+		
+		if(b!=null){result = result - b;}
+		
+		if(result!=null){result = AmountRounder.two(result);}
+		return result;
+	}
 }

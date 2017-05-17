36c80c90e383fba9043be6b358039c331819cd7c
Thorsten <t.kisner@web.de>
2017-05-17 14:34:16 +0200

    winaudit.xml test file added

--- util/src/test/java/org/jeesl/controller/processor/CliInventoryProcessor.java
+++ util/src/test/java/org/jeesl/controller/processor/CliInventoryProcessor.java
@@ -1,34 +1,28 @@
 package org.jeesl.controller.processor;
 
-import java.io.File;
-
-import org.apache.commons.configuration.Configuration;
 import org.jdom2.Document;
 import org.jeesl.JeeslUtilTestBootstrap;
 import org.jeesl.model.xml.module.inventory.Inventory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.sf.exlp.util.io.resourceloader.MultiResourceLoader;
 import net.sf.exlp.util.xml.JDomUtil;
 import net.sf.exlp.util.xml.JaxbUtil;
 
-
-public class CliInventoryProcessor {
-	
+public class CliInventoryProcessor
+{
 	final static Logger logger = LoggerFactory.getLogger(CliInventoryProcessor.class);
 	
     public static void main(String[] args) throws Exception
     {
-    	Configuration config = JeeslUtilTestBootstrap.init();
+    	JeeslUtilTestBootstrap.init();
 
-    	File f = new File(config.getString("cli.inventory.pc.winaudit"));
-    	Inventory i = JaxbUtil.loadJAXB(f, Inventory.class);
+    	MultiResourceLoader mrl = new MultiResourceLoader();
+    	Inventory i = JaxbUtil.loadJAXB(mrl.searchIs("data/processor/inventory/winaudit.xml"), Inventory.class);
     	JaxbUtil.info(i);
-    	
-    	    Document doc = JDomUtil.load(f);
-    	    JDomUtil.debug(doc);
+
+    	Document doc = JDomUtil.load(mrl.searchIs("data/processor/inventory/winaudit.xml"));
+    	JDomUtil.debug(doc);
     }
-   
 }
-        
-    

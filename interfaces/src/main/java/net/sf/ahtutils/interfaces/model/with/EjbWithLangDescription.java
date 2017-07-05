16084cdecab99dce2d5f08ed263adabcbd087741
Thorsten-K <t.kisner@web.de>
2017-07-05 12:24:15 +0200

    lang/inputRows added

--- interfaces/src/main/java/net/sf/ahtutils/interfaces/model/with/EjbWithLangDescription.java
+++ interfaces/src/main/java/net/sf/ahtutils/interfaces/model/with/EjbWithLangDescription.java
@@ -6,7 +6,8 @@ import net.sf.ahtutils.model.interfaces.with.EjbWithDescription;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 import net.sf.ahtutils.model.interfaces.with.EjbWithLang;
 
-public interface EjbWithLangDescription<L extends UtilsLang, D extends UtilsDescription> extends EjbWithId,EjbWithLang<L>,EjbWithDescription<D>
+public interface EjbWithLangDescription<L extends UtilsLang, D extends UtilsDescription>
+					extends EjbWithId, EjbWithLang<L>, EjbWithDescription<D>
 {
 
 }

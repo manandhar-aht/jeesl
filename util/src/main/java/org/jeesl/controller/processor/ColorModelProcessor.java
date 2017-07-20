43ec545479b410f58170d6554d20de0f1ad86ebb
Lasse <lasse-petersen@t-online.de>
2017-07-20 10:40:38 +0200

    ColorModelProcessor fixed

--- util/src/main/java/org/jeesl/controller/processor/ColorModelProcessor.java
+++ util/src/main/java/org/jeesl/controller/processor/ColorModelProcessor.java
@@ -5,58 +5,89 @@ import java.awt.Color;
 public class ColorModelProcessor
 
 {
-	private static double[] hsv;
-	private static double[] rgb;
-	private static float[] hsb;
-	@SuppressWarnings("unused")
-	private static String inRGBString;
-	private static int inRED, inGREEN, inBLUE;
+	private double[] hsv;
+	private double[] rgb;
+	private float[] hsb;
+	private int inRED, inGREEN, inBLUE;
 	
-	public ColorModelProcessor()
+	/**
+	 * edit the hue, saturation or value (HSV) of an RGB color (red,green,blue)
+	 * and get back RGB 
+	 * 
+	 * @param rgbString
+	 *            - RGB-color-code like "FF00AA"
+	 * 
+	 */
+	public ColorModelProcessor(String rgbString)
 	{
-	}
-	
-	public static double getHUE()
-	{
-		return hsv[0];
-	}
-	
-	public static double getSAT()
-	{
-		return hsv[1];
-	}
-	
-	public static double getVAL()
-	{
-		return hsv[2];
-	}
-	
-	public static void setHUE(double hue)
-	{
-		ColorModelProcessor.hsv[0] = hue;
-	}
-	
-	public static void setSAT(double sat)
-	{
-		ColorModelProcessor.hsv[1] = sat;
-	}
-	
-	public static void setVAL(double val)
-	{
-		ColorModelProcessor.hsv[2] = val;
-	}
-	
-	public static void init(String rgbString)
-	{
-		ColorModelProcessor.inRGBString = rgbString;
-		ColorModelProcessor.inRED = Integer.parseInt(rgbString.substring(0, 2), 16);
-		ColorModelProcessor.inGREEN = Integer.parseInt(rgbString.substring(2, 4), 16);
-		ColorModelProcessor.inBLUE = Integer.parseInt(rgbString.substring(4, 6), 16);
+		this.inRED = Integer.parseInt(rgbString.substring(0, 2), 16);
+		this.inGREEN = Integer.parseInt(rgbString.substring(2, 4), 16);
+		this.inBLUE = Integer.parseInt(rgbString.substring(4, 6), 16);
 		RGB2HSV(inRED, inGREEN, inBLUE);
 		HSVtoRGB(hsv[0], hsv[1], hsv[2]);
 	}
 	
-	public static double[] RGB2HSV(double r, double g, double b)
+	/**
+	 * @return double - actual Hue
+	 */
+	public double getHUE()
+	{
+		return hsv[0];
+	}
+	
+	/**
+	 * @return double - actual saturation
+	 */
+	public double getSAT()
+	{
+		return hsv[1];
+	}
+	
+	/**
+	 * @return double - actual value
+	 */
+	public double getVAL()
+	{
+		return hsv[2];
+	}
+	
+	/**
+	 * @param hue
+	 *            - set hue (0-100)
+	 */
+	public void setHUE(double hue)
+	{
+		this.hsv[0] = hue;
+	}
+	
+	/**
+	 * @param sat
+	 *            - set saturation (0-100)
+	 */
+	public void setSAT(double sat)
+	{
+		this.hsv[1] = sat;
+	}
+	
+	/**
+	 * @param val
+	 *            - set value (0-100)
+	 */
+	public void setVAL(double val)
+	{
+		this.hsv[2] = val;
+	}
+	
+	/**
+	 * @param r
+	 *            - set red (0-255)
+	 * @param g
+	 *            - set green (0-255)
+	 * @param b
+	 *            - set blue (0-255)
+	 * @return double[] - hue, saturation, value
+	 */
+	public double[] RGB2HSV(double r, double g, double b)
 	{
 		hsv = new double[3];
 		hsb = Color.RGBtoHSB((int) r, (int) g, (int) b, null);
@@ -69,7 +100,16 @@ public class ColorModelProcessor
 		return hsv;
 	}
 	
-	public static double[] HSVtoRGB(double h, double s, double v)
+	/**
+	 * @param h
+	 *            - set hue (0-100)
+	 * @param s
+	 *            - set saturation (0-100)
+	 * @param v
+	 *            - set value (0-100)
+	 * @return double[] - red, green, blue
+	 */
+	public double[] HSVtoRGB(double h, double s, double v)
 	{
 		
 		rgb = new double[3];
@@ -83,13 +123,16 @@ public class ColorModelProcessor
 		return rgb;
 	}
 	
-	public static String getNewRGBString()
-	{	
-		HSVtoRGB(hsv[0],hsv[1],hsv[2]);
+	/**
+	 * @return String new RGB String like "FF8822"
+	 */
+	public String getNewRGBString()
+	{
+		HSVtoRGB(hsv[0], hsv[1], hsv[2]);
 		StringBuilder sb = new StringBuilder();
-		sb.append(Integer.toHexString((int)Math.round(rgb[0])));
-		sb.append(Integer.toHexString((int)Math.round(rgb[1])));
-		sb.append(Integer.toHexString((int)Math.round(rgb[2])));
+		sb.append(Integer.toHexString((int) Math.round(rgb[0])));
+		sb.append(Integer.toHexString((int) Math.round(rgb[1])));
+		sb.append(Integer.toHexString((int) Math.round(rgb[2])));
 		return sb.toString();
 	}
 }

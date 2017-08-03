874eac4f5a9072fb38a897fb7c5b566d3cda935b
Thorsten-K <t.kisner@web.de>
2017-08-03 05:29:50 +0200

    tree node added

--- prototype/src/main/java/org/jeesl/web/mbean/prototype/admin/system/io/AbstractCmsBean.java
+++ prototype/src/main/java/org/jeesl/web/mbean/prototype/admin/system/io/AbstractCmsBean.java
@@ -2,6 +2,9 @@ package org.jeesl.web.mbean.prototype.admin.system.io;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
 import org.jeesl.api.facade.io.JeeslIoCmsFacade;
 import org.jeesl.controller.handler.sb.SbSingleHandler;
 import org.jeesl.factory.ejb.system.io.cms.EjbIoCmsFactory;
@@ -14,6 +17,9 @@ import org.jeesl.interfaces.model.system.io.cms.JeeslIoCmsElement;
 import org.jeesl.interfaces.model.system.io.cms.JeeslIoCmsSection;
 import org.jeesl.interfaces.model.system.io.cms.JeeslIoCmsVisiblity;
 import org.jeesl.web.mbean.prototype.admin.AbstractAdminBean;
+import org.primefaces.event.TreeDragDropEvent;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -54,6 +60,23 @@ public abstract class AbstractCmsBean <L extends UtilsLang,D extends UtilsDescri
 	protected CMS cms; public CMS getCms() {return cms;} public void setCms(CMS cms) {this.cms = cms;}
 	protected CAT category;
 	
+	private TreeNode root1;
+
+     
+    private TreeNode selectedNode1;
+    public TreeNode getRoot1() {
+        return root1;
+    }
+ 
+    public TreeNode getSelectedNode1() {
+        return selectedNode1;
+    }
+ 
+    public void setSelectedNode1(TreeNode selectedNode1) {
+        this.selectedNode1 = selectedNode1;
+    }
+     
+
 	public AbstractCmsBean(final Class<L> cL, final Class<D> cD, final Class<CMS> cCms)
 	{
 		super(cL,cD);
@@ -69,6 +92,25 @@ public abstract class AbstractCmsBean <L extends UtilsLang,D extends UtilsDescri
 		super.initAdmin(langs,cL,cD,bMessage);
 		this.fCms=fCms;
 		reloadCms();
+		
+		root1 = new DefaultTreeNode("Root", null);
+        TreeNode node0 = new DefaultTreeNode("Node 0", root1);
+        TreeNode node1 = new DefaultTreeNode("Node 1", root1);
+        TreeNode node2 = new DefaultTreeNode("Node 2", root1);
+         
+        TreeNode node00 = new DefaultTreeNode("Node 0.0", node0);
+        TreeNode node01 = new DefaultTreeNode("Node 0.1", node0);
+         
+        TreeNode node10 = new DefaultTreeNode("Node 1.0", node1);
+        TreeNode node11 = new DefaultTreeNode("Node 1.1", node1);
+         
+        TreeNode node000 = new DefaultTreeNode("Node 0.0.0", node00);
+        TreeNode node001 = new DefaultTreeNode("Node 0.0.1", node00);
+        TreeNode node010 = new DefaultTreeNode("Node 0.1.0", node01);
+         
+        TreeNode node100 = new DefaultTreeNode("Node 1.0.0", node10);
+		
+		
 		sbhCms.silentCallback();
 	}
 	
@@ -113,4 +155,13 @@ public abstract class AbstractCmsBean <L extends UtilsLang,D extends UtilsDescri
 		cms = fCms.save(cms);
 		reloadCms();
 	}
+	
+	public void onDragDrop(TreeDragDropEvent event) {
+        TreeNode dragNode = event.getDragNode();
+        TreeNode dropNode = event.getDropNode();
+        int dropIndex = event.getDropIndex();
+         
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Dragged " + dragNode.getData(), "Dropped on " + dropNode.getData() + " at " + dropIndex);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
 }

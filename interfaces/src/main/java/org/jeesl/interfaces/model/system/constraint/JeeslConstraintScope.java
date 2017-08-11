2eab46f64e5294de118ed21dc1430f88bf4fec26
Thorsten-K <t.kisner@web.de>
2017-08-11 07:37:51 +0200

    JeeslConstraint added

--- interfaces/src/main/java/org/jeesl/interfaces/model/system/constraint/JeeslConstraintScope.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/system/constraint/JeeslConstraintScope.java
@@ -1,4 +1,4 @@
-package org.jeesl.interfaces.model.system.util;
+package org.jeesl.interfaces.model.system.constraint;
 
 import net.sf.ahtutils.interfaces.model.behaviour.EjbSaveable;
 import net.sf.ahtutils.interfaces.model.crud.EjbRemoveable;
@@ -6,22 +6,18 @@ import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
 import net.sf.ahtutils.interfaces.model.with.EjbWithLangDescription;
-import net.sf.ahtutils.interfaces.model.with.status.UtilsWithScope;
+import net.sf.ahtutils.interfaces.model.with.position.EjbWithPosition;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 
-public interface JeeslTrafficLight<L extends UtilsLang, D extends UtilsDescription,
-								SCOPE extends UtilsStatus<SCOPE,L,D>>
+public interface JeeslConstraintScope<L extends UtilsLang, D extends UtilsDescription,
+									SCOPE extends JeeslConstraintScope<L,D,SCOPE,CATEGORY,CONSTRAINT,TYPE>,
+									CATEGORY extends UtilsStatus<CATEGORY,L,D>,
+									CONSTRAINT extends JeeslConstraint<L,D,SCOPE,CATEGORY,CONSTRAINT,TYPE>,
+									TYPE extends UtilsStatus<TYPE,L,D>>
 			extends EjbWithId,
 					EjbSaveable,EjbRemoveable,
-					UtilsWithScope<L,D,SCOPE>,
+					EjbWithPosition,
 					EjbWithLangDescription<L,D>
 {
-	double getThreshold();
-	void setThreshold(double threshold);
-	
-	String getColorBackground();
-	void setColorBackground(String colorBackground);
-	
-	String getColorText();
-	void setColorText(String colorText);
+
 }

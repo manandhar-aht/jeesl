d286d9be365c66f9dd37c91cdf7a61ec7c7aee4b
Thorsten-K <t.kisner@web.de>
2017-10-24 07:44:08 +0200

    SecurityFactoryBuilder with generics

--- util/src/main/java/org/jeesl/factory/builder/SecurityFactoryBuilder.java
+++ util/src/main/java/org/jeesl/factory/builder/SecurityFactoryBuilder.java
@@ -2,6 +2,7 @@ package org.jeesl.factory.builder;
 
 import org.jeesl.factory.ejb.system.security.EjbSecurityActionFactory;
 import org.jeesl.factory.ejb.system.security.EjbSecurityMenuFactory;
+import org.jeesl.factory.ejb.system.security.EjbSecurityViewFactory;
 import org.jeesl.factory.txt.system.security.TxtStaffFactory;
 import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityAction;
 import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityCategory;
@@ -31,28 +32,29 @@ public class SecurityFactoryBuilder<L extends UtilsLang, D extends UtilsDescript
 {
 	final static Logger logger = LoggerFactory.getLogger(SecurityFactoryBuilder.class);
         
-    private final Class<A> cAction;
+	private final Class<C> cCategory; public Class<C> getClassCategory(){return cCategory;}
+	private final Class<R> cRole; public Class<R> getClassRole(){return cRole;}
+	private final Class<V> cView; public Class<V> getClassView(){return cView;}
+	private final Class<U> cUsecase; public Class<U> getClassUsecase(){return cUsecase;}
+    private final Class<A> cAction; public Class<A> getClassAction(){return cAction;}
+    private final Class<AT> cTemplate; public Class<AT> getClassTemplate(){return cTemplate;}
+    private final Class<USER> cUser; public Class<USER> getClassUser(){return cUser;}
 	
-	public SecurityFactoryBuilder(final Class<L> cL, final Class<D> cD, final Class<A> cAction)
+	public SecurityFactoryBuilder(final Class<L> cL, final Class<D> cD, final Class<C> cCategory, final Class<R> cRole, final Class<V> cView, final Class<U> cUsecase, final Class<A> cAction, final Class<AT> cTemplate, final Class<USER> cUser)
 	{		
 		super(cL,cD);
-		
+		this.cCategory=cCategory;
+		this.cRole=cRole;
+		this.cView=cView;
+		this.cUsecase=cUsecase;
 		this.cAction=cAction;
+		this.cTemplate=cTemplate;
+		this.cUser=cUser;
 	}
-	
-	public static <L extends UtilsLang,
-					D extends UtilsDescription,
-					C extends JeeslSecurityCategory<L,D>,
-					R extends JeeslSecurityRole<L,D,C,R,V,U,A,AT,USER>,
-					V extends JeeslSecurityView<L,D,C,R,V,U,A,AT,USER>,
-					U extends JeeslSecurityUsecase<L,D,C,R,V,U,A,AT,USER>,
-					A extends JeeslSecurityAction<L,D,C,R,V,U,A,AT,USER>,
-					AT extends JeeslSecurityTemplate<L,D,C,R,V,U,A,AT,USER>,
-					USER extends JeeslUser<L,D,C,R,V,U,A,AT,USER>>
-		SecurityFactoryBuilder<L,D,C,R,V,U,A,AT,USER>
-		factory(final Class<L> cL, final Class<D> cD, final Class<A> cAction)
+		
+	public EjbSecurityViewFactory<L,D,C,R,V,U,A,AT,USER> ejbView()
 	{
-		return new SecurityFactoryBuilder<L,D,C,R,V,U,A,AT,USER>(cL,cD,cAction);
+		return new EjbSecurityViewFactory<L,D,C,R,V,U,A,AT,USER>(cL,cD,cCategory,cRole,cView,cUsecase,cAction,cUser);
 	}
 	
 	public EjbSecurityActionFactory<L,D,C,R,V,U,A,AT,USER> ejbAction()

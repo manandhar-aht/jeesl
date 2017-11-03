465557a2160a06d14e86fc9f538fbd209b7cbeb0
Thorsten-K <t.kisner@web.de>
2017-11-03 13:24:30 +0200

    refId changed from Long to long

--- interfaces/src/main/java/org/jeesl/interfaces/model/module/survey/core/JeeslSurveyTemplateVersion.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/survey/core/JeeslSurveyTemplateVersion.java
@@ -1,24 +1,24 @@
 package org.jeesl.interfaces.model.module.survey.core;
 
+import java.io.Serializable;
+
 import net.sf.ahtutils.interfaces.model.behaviour.EjbSaveable;
+import net.sf.ahtutils.interfaces.model.crud.EjbRemoveable;
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
+import net.sf.ahtutils.interfaces.model.with.EjbWithRefId;
 import net.sf.ahtutils.model.interfaces.with.EjbWithDescription;
-import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 import net.sf.ahtutils.model.interfaces.with.EjbWithLang;
 import net.sf.ahtutils.model.interfaces.with.EjbWithRecord;
 
 public interface JeeslSurveyTemplateVersion<L extends UtilsLang, D extends UtilsDescription,
 											TEMPLATE extends JeeslSurveyTemplate<L,D,?,TEMPLATE,?,?,?,?,?,?>>
-			extends EjbWithId,EjbSaveable,
-					EjbWithRecord,//EjbWithRefId,
+			extends Serializable,EjbSaveable,EjbRemoveable,
+					EjbWithRecord,EjbWithRefId,
 					EjbWithLang<L>,EjbWithDescription<D>
 {
 	enum Attributes {template,record}
 	
 	TEMPLATE getTemplate();
 	void setTemplate(TEMPLATE template);
-	
-	Long getRefId();
-	void setRefId(Long refId);
 }

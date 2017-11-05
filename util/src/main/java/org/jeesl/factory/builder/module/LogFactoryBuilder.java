97da3193ac352b7919150af63f3400f0803392bd
Thorsten-K <t.kisner@web.de>
2017-11-05 09:51:35 +0100

    LogFactoryFactory moved to builder

--- util/src/main/java/org/jeesl/factory/builder/module/LogFactoryBuilder.java
+++ util/src/main/java/org/jeesl/factory/builder/module/LogFactoryBuilder.java
@@ -1,5 +1,6 @@
-package org.jeesl.factory.factory;
+package org.jeesl.factory.builder.module;
 
+import org.jeesl.factory.builder.AbstractFactoryBuilder;
 import org.jeesl.factory.ejb.module.log.EjbLogFactory;
 import org.jeesl.factory.ejb.module.log.EjbLogItemFactory;
 import org.jeesl.interfaces.model.module.log.JeeslLog;
@@ -12,40 +13,25 @@ import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 
-public class LogFactoryFactory<L extends UtilsLang, D extends UtilsDescription,
+public class LogFactoryBuilder<L extends UtilsLang, D extends UtilsDescription,
 								LOG extends JeeslLog<L,D,LOG,ITEM,IMPACT,SCOPE,USER>,
 								ITEM extends JeeslLogItem<L,D,LOG,ITEM,IMPACT,SCOPE,USER>,
 								IMPACT extends UtilsStatus<IMPACT,L,D>,
 								SCOPE extends UtilsStatus<SCOPE,L,D>,
 								USER extends EjbWithId
 								>
+	extends AbstractFactoryBuilder<L,D>
 {
-	final static Logger logger = LoggerFactory.getLogger(LogFactoryFactory.class);
+	final static Logger logger = LoggerFactory.getLogger(LogFactoryBuilder.class);
 	
-//	private final Class<L> cL;
-//	private final Class<D> cD;
 	private final Class<LOG> cLog;
 	private final Class<ITEM> cItem;
 
-    
-	private LogFactoryFactory(final Class<L> cL,final Class<D> cD,final Class<LOG> cLog, final Class<ITEM> cItem)
+	public LogFactoryBuilder(final Class<L> cL,final Class<D> cD,final Class<LOG> cLog, final Class<ITEM> cItem)
 	{       
-//		this.cL = cL;
-//       this.cD = cD;
+		super(cL,cD);
         this.cLog = cLog;
         this.cItem = cItem;
-	}
-	
-	public static <L extends UtilsLang, D extends UtilsDescription,
-					LOG extends JeeslLog<L,D,LOG,ITEM,IMPACT,SCOPE,USER>,
-					ITEM extends JeeslLogItem<L,D,LOG,ITEM,IMPACT,SCOPE,USER>,
-					IMPACT extends UtilsStatus<IMPACT,L,D>,
-					SCOPE extends UtilsStatus<SCOPE,L,D>,
-					USER extends EjbWithId
-					>
-		LogFactoryFactory<L,D,LOG,ITEM,IMPACT,SCOPE,USER> factory(final Class<L> cL,final Class<D> cD,final Class<LOG> cLog, final Class<ITEM> cItem)
-	{
-		return new LogFactoryFactory<L,D,LOG,ITEM,IMPACT,SCOPE,USER>(cL,cD,cLog,cItem);
 	}
 	
 	public EjbLogFactory<L,D,LOG,ITEM,IMPACT,SCOPE,USER> log(){return new EjbLogFactory<L,D,LOG,ITEM,IMPACT,SCOPE,USER>(cLog);}

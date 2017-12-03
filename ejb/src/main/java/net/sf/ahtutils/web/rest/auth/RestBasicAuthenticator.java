23d66b286ad704a918e2b504057eebaed92f1e1b
Thorsten-K <t.kisner@web.de>
2017-12-03 16:56:51 +0100

    JeeslRest Secured/LoggedIn added

--- ejb/src/main/java/net/sf/ahtutils/web/rest/auth/RestBasicAuthenticator.java
+++ ejb/src/main/java/net/sf/ahtutils/web/rest/auth/RestBasicAuthenticator.java
@@ -17,22 +17,22 @@ public class RestBasicAuthenticator
 	public static UtilsCredential decodeResteasy(HttpRequest request) throws UnauthorizedException
 	{
 		List<String> header = request.getHttpHeaders().getRequestHeader("Authorization");
-    	if(header!=null)
-    	{
-    		String auth = header.get(0);
-    		if(auth.toUpperCase().startsWith("BASIC "))
-    		{
-    			String base64 = auth.substring("BASIC ".length());
-    			logger.trace("Base64: "+base64);
-        		String token = new String(Base64.decodeBase64(base64));
-            	logger.trace("Authorization: "+token);
-            	
-            	int index = token.indexOf(":");
-            	
-            	return new UtilsCredential(token.substring(0,index),token.substring(index+1));
-    		}
-    		else {throw new UnauthorizedException("We only support BASIC authentication");}
-    	}
-    	else{throw new UnauthorizedException("No Authorization Header available");}
+	    	if(header!=null)
+	    	{
+	    		String auth = header.get(0);
+	    		if(auth.toUpperCase().startsWith("BASIC "))
+	    		{
+	    			String base64 = auth.substring("BASIC ".length());
+	    			logger.trace("Base64: "+base64);
+	        		String token = new String(Base64.decodeBase64(base64));
+	            	logger.trace("Authorization: "+token);
+	            	
+	            	int index = token.indexOf(":");
+	            	
+	            	return new UtilsCredential(token.substring(0,index),token.substring(index+1));
+	    		}
+	    		else {throw new UnauthorizedException("We only support BASIC authentication");}
+	    	}
+	    	else{throw new UnauthorizedException("No Authorization Header available");}
 	}
 }

3ae9d9c8e02e1c04df4a7c591612efa0b89f7dd6
Lasse <Lasse@DESKTOP-PGN2LJG.Speedport_W_724V_09011603_05_012>
2017-12-07 21:33:53 +0100

    added 2nd constructor to PostgresDump to support different db's

--- util/src/main/java/net/sf/ahtutils/db/shell/postgres/PostgresDump.java
+++ util/src/main/java/net/sf/ahtutils/db/shell/postgres/PostgresDump.java
@@ -1,6 +1,7 @@
 package net.sf.ahtutils.db.shell.postgres;
 
 import java.io.File;
+
 import java.util.NoSuchElementException;
 
 import org.apache.commons.configuration.Configuration;
@@ -14,16 +15,27 @@ import net.sf.exlp.factory.xml.config.XmlParameterFactory;
 public class PostgresDump extends AbstractPostgresShell implements UtilsDbShell
 {
 	final static Logger logger = LoggerFactory.getLogger(PostgresDump.class);
-	
+	String db;
 	public PostgresDump(Configuration config)
-    {
+	{
 		super(config,UtilsDbShell.Operation.dump);
 		
 		pDbShell = XmlParameterFactory.build(UtilsDbShell.cfgBinDump, "Shell command for dump", false);
 		try{pDbShell.setValue(config.getString(pDbShell.getKey()));}
 		catch (NoSuchElementException e){pDbShell.setValue("pg_dump");}
 		configurationParamter.getParameter().add(pDbShell);
-    }
+	}
+	
+	/**
+	 * added 2nd constructor for different db's
+	 * @config normal config file
+	 * @db String for db declaration / wis=mopwh
+	 */
+	public PostgresDump(Configuration config, String db)
+	{
+	    this(config);
+	    this.db=db; 	
+	}
 	
 	public void writeShell() throws ExlpUnsupportedOsException
 	{
@@ -35,6 +47,8 @@ public class PostgresDump extends AbstractPostgresShell implements UtilsDbShell
 	public void buildCommands(boolean withStructure) throws ExlpUnsupportedOsException
 	{
 		super.cmdPre();
+		if (db=="wis") {dumpDatabaseWis();}
+		else
 		dumpDatabase();
 		super.cmdPost();
 	}
@@ -48,6 +62,33 @@ public class PostgresDump extends AbstractPostgresShell implements UtilsDbShell
 		sb.append(" --create");
 		sb.append(" --blobs");
 		sb.append(" --format=c");
+		sb.append(" --verbose");
+		sb.append(" --file=").append(pDirDump.getValue()+File.separator+pDbName.getValue()+".sql");
+		sb.append(" ").append(pDbName.getValue());
+		
+		super.addLine(sb.toString());
+		return sb.toString();
+	}
+	
+	/**
+	 *  added extra options to dump wis db for restoring geo*tables+seq correctly 
+	 */
+	private String dumpDatabaseWis()
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append(pDbShell.getValue());
+		sb.append(" -h ").append(pDbHost.getValue());
+		sb.append(" -U ").append(pDbUser.getValue());
+		sb.append(" --quote-all-identifiers");
+		sb.append(" --blobs");
+		sb.append(" --format=c");
+		sb.append(" --role \"postgres\"");
+		sb.append(" --section=pre-data");
+		sb.append(" --section=data");
+		sb.append(" --section=post-data");
+		sb.append(" --oids");
+		sb.append(" --use-set-session-authorization");
+		sb.append(" --encoding \"UTF8\"");
 		sb.append(" --verbose");
 		sb.append(" --file=").append(pDirDump.getValue()+File.separator+pDbName.getValue()+".sql");
 		sb.append(" ").append(pDbName.getValue());

6c3a835482a79ab8aa9c3e5eb410a5a373787d15
Thorsten-K <t.kisner@web.de>
2018-01-17 09:20:06 +0100

    UtilsFixedCode added to RevisionEntityRelation

--- interfaces/src/main/java/org/jeesl/interfaces/model/system/io/revision/JeeslRevisionEntityRelation.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/system/io/revision/JeeslRevisionEntityRelation.java
@@ -10,12 +10,13 @@ import net.sf.ahtutils.interfaces.model.crud.EjbPersistable;
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatusFixedCode;
 import net.sf.ahtutils.interfaces.model.status.UtilsWithSymbol;
 
 public interface JeeslRevisionEntityRelation <S extends UtilsStatus<S,L,D>,
 												L extends UtilsLang, D extends UtilsDescription,
 												G extends JeeslGraphic<L,D,G,?,?,?>>
-		extends Serializable,EjbPersistable,UtilsWithSymbol,JeeslOptionRestDownload,EjbWithCodeGraphic<G>
+		extends Serializable,EjbPersistable,UtilsWithSymbol,JeeslOptionRestDownload,EjbWithCodeGraphic<G>,UtilsStatusFixedCode
 {	
 	public enum Code{MtoO,OtoO,OtoM,MtoM}
 }

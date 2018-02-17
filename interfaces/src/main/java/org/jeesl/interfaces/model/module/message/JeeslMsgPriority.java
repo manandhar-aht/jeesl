d5269b6651c28a0a172d2199df5c6c61dcea33e3
Thorsten-K <t.kisner@web.de>
2018-02-17 07:33:34 +0100

    JeeslMsgPriority added

---
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/message/JeeslMsgPriority.java
@@ -0,0 +1,23 @@
+package org.jeesl.interfaces.model.module.message;
+
+import java.io.Serializable;
+
+import org.jeesl.interfaces.model.system.graphic.with.EjbWithCodeGraphic;
+import org.jeesl.interfaces.model.system.option.JeeslOptionRestDownload;
+import org.jeesl.interfaces.model.system.symbol.JeeslGraphic;
+import org.jeesl.interfaces.model.system.with.code.EjbWithCode;
+
+import net.sf.ahtutils.interfaces.model.crud.EjbPersistable;
+import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
+import net.sf.ahtutils.interfaces.model.status.UtilsLang;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatusFixedCode;
+
+public interface JeeslMsgPriority <S extends UtilsStatus<S,L,D>, L extends UtilsLang, D extends UtilsDescription,G extends JeeslGraphic<L,D,G,?,?,?>>
+					extends Serializable,EjbPersistable,
+								EjbWithCode,UtilsStatusFixedCode,
+								JeeslOptionRestDownload,EjbWithCodeGraphic<G>,
+								UtilsStatus<S,L,D>
+{	
+	public enum Code{low,medium,high}
+}

6f342e20cb8df08b304aba0d775a4fe4edfcf5bd
Thorsten-K <t.kisner@web.de>
2018-02-18 19:09:34 +0100

    PasswordRating improved

--- interfaces/src/main/java/org/jeesl/interfaces/model/system/security/framework/JeeslSecurityPasswordRating.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/system/security/framework/JeeslSecurityPasswordRating.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import org.jeesl.interfaces.model.system.graphic.with.EjbWithCodeGraphic;
 import org.jeesl.interfaces.model.system.option.JeeslOptionRestDownload;
 import org.jeesl.interfaces.model.system.symbol.JeeslGraphic;
+import org.jeesl.interfaces.model.system.with.code.EjbWithCode;
 
 import net.sf.ahtutils.interfaces.model.crud.EjbPersistable;
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
@@ -14,7 +15,10 @@ import net.sf.ahtutils.interfaces.model.status.UtilsStatusFixedCode;
 
 public interface JeeslSecurityPasswordRating <S extends UtilsStatus<S,L,D>,
 									L extends UtilsLang, D extends UtilsDescription,G extends JeeslGraphic<L,D,G,?,?,?>>
-		extends Serializable,EjbPersistable,JeeslOptionRestDownload,UtilsStatusFixedCode,EjbWithCodeGraphic<G>
+		extends Serializable,EjbPersistable,
+					EjbWithCode,UtilsStatusFixedCode,
+					JeeslOptionRestDownload,EjbWithCodeGraphic<G>,
+					UtilsStatus<S,L,D>
 {
 	public enum Code{r0,r1,r2,r3,r4}
 	public static final String codePrefix = "r";

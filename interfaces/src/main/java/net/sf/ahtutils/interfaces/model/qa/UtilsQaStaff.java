55e43c001fd4d7fedde89985b2d608e42db36d1e
Thorsten-K <t.kisner@web.de>
2018-03-05 16:09:12 +0100

    Reducing QA generics for QaScheduleSlot

--- interfaces/src/main/java/net/sf/ahtutils/interfaces/model/qa/UtilsQaStaff.java
+++ interfaces/src/main/java/net/sf/ahtutils/interfaces/model/qa/UtilsQaStaff.java
@@ -1,47 +1,23 @@
 package net.sf.ahtutils.interfaces.model.qa;
 
+import java.io.Serializable;
 import java.util.List;
 
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityView;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityTemplate;
+import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityRole;
 import org.jeesl.interfaces.model.system.security.user.JeeslUser;
 import org.jeesl.interfaces.model.system.security.util.JeeslStaff;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityAction;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityUsecase;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityCategory;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityRole;
 
-import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
-import net.sf.ahtutils.interfaces.model.status.UtilsLang;
-import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
+import net.sf.ahtutils.interfaces.model.behaviour.EjbSaveable;
+import net.sf.ahtutils.interfaces.model.crud.EjbPersistable;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 
-public interface UtilsQaStaff<L extends UtilsLang,
-					D extends UtilsDescription,
-					C extends JeeslSecurityCategory<L,D>,
-					R extends JeeslSecurityRole<L,D,C,V,U,A,USER>,
-					V extends JeeslSecurityView<L,D,C,R,U,A>,
-					U extends JeeslSecurityUsecase<L,D,C,R,V,A>,
-					A extends JeeslSecurityAction<L,D,R,V,U,AT>,
-					AT extends JeeslSecurityTemplate<L,D,C>,
+public interface UtilsQaStaff<
+					R extends JeeslSecurityRole<?,?,?,?,?,?,USER>,
 					USER extends JeeslUser<R>,
-					STAFF extends UtilsQaStaff<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					GROUP extends UtilsQaGroup<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QA extends UtilsQualityAssurarance<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QASD extends UtilsQaSchedule<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QASS extends UtilsQaScheduleSlot<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QAC extends UtilsQaCategory<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QAT extends UtilsQaTest<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QAU extends UtilsQaUsability<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QAR extends UtilsQaResult<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QASH extends UtilsQaStakeholder<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QATD extends UtilsQaTestDiscussion<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QATI extends UtilsQaTestInfo<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-					QATC extends UtilsStatus<QATC,L,D>,
-					QATS extends UtilsStatus<QATS,L,D>,
-					QARS extends UtilsStatus<QARS,L,D>,
-					QAUS extends UtilsStatus<QAUS,L,D>>
-			extends EjbWithId,JeeslStaff<R,USER,QA,QA>
+					GROUP extends UtilsQaGroup<?,QA,?>,
+					QA extends UtilsQualityAssurarance<?,?,QASH>,
+					QASH extends UtilsQaStakeholder<?,?,?,R,?,?,?,?,USER,?,GROUP,QA,?,?,?,?,?,?,QASH,?,?,?,?,?,?>>
+			extends Serializable,EjbSaveable,EjbPersistable,EjbWithId,JeeslStaff<R,USER,QA,QA>
 {
 	QASH getStakeholder();
 	void setStakeholder(QASH stakeholder);

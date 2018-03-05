b0a74f2a880f0158ab26fc9c644908b553b4ebb1
Thorsten-K <t.kisner@web.de>
2018-03-05 17:05:06 +0100

    Reducing generics of QaUsability

--- interfaces/src/main/java/net/sf/ahtutils/interfaces/model/qa/UtilsQaUsability.java
+++ interfaces/src/main/java/net/sf/ahtutils/interfaces/model/qa/UtilsQaUsability.java
@@ -1,44 +1,11 @@
 package net.sf.ahtutils.interfaces.model.qa;
 
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityView;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityTemplate;
-import org.jeesl.interfaces.model.system.security.user.JeeslUser;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityAction;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityUsecase;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityCategory;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityRole;
+import java.io.Serializable;
 
-import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
-import net.sf.ahtutils.interfaces.model.status.UtilsLang;
-import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 
-public interface UtilsQaUsability<L extends UtilsLang,
-				D extends UtilsDescription,
-				C extends JeeslSecurityCategory<L,D>,
-				R extends JeeslSecurityRole<L,D,C,V,U,A,USER>,
-				V extends JeeslSecurityView<L,D,C,R,U,A>,
-				U extends JeeslSecurityUsecase<L,D,C,R,V,A>,
-				A extends JeeslSecurityAction<L,D,R,V,U,AT>,
-				AT extends JeeslSecurityTemplate<L,D,C>,
-				USER extends JeeslUser<R>,
-				STAFF extends UtilsQaStaff<R,USER,GROUP,QA,QASH>,
-				GROUP extends UtilsQaGroup<STAFF,QA,QASS>,
-				QA extends UtilsQualityAssurarance<STAFF,QAC,QASH>,
-				QASD extends UtilsQaSchedule<QA,QASS>,
-				QASS extends UtilsQaScheduleSlot<GROUP,QASD>,
-				QAC extends UtilsQaCategory<QA,QAT>,
-				QAT extends UtilsQaTest<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-				QAU extends UtilsQaUsability<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-				QAR extends UtilsQaResult<STAFF,QAT,QARS>,
-				QASH extends UtilsQaStakeholder<QA>,
-				QATD extends UtilsQaTestDiscussion<STAFF,QAT>,
-				QATI extends UtilsQaTestInfo<L,D,C,R,V,U,A,AT,USER,STAFF,GROUP,QA,QASD,QASS,QAC,QAT,QAU,QAR,QASH,QATD,QATI,QATC,QATS,QARS,QAUS>,
-				QATC extends UtilsStatus<QATC,L,D>,
-				QATS extends UtilsStatus<QATS,L,D>,
-				QARS extends UtilsStatus<QARS,L,D>,
-				QAUS extends UtilsStatus<QAUS,L,D>>
-			extends EjbWithId
+public interface UtilsQaUsability
+			extends Serializable,EjbWithId
 {
 	
 }

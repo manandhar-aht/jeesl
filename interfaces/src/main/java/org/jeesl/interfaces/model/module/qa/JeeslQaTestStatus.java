290c8cf36a2f3d16f533a140ad169f54299da684
Thorsten-K <t.kisner@web.de>
2018-03-05 14:11:59 +0100

    QA interfaces added

---
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/qa/JeeslQaTestStatus.java
@@ -0,0 +1,19 @@
+package org.jeesl.interfaces.model.module.qa;
+
+import java.io.Serializable;
+
+import org.jeesl.interfaces.model.system.graphic.with.EjbWithCodeGraphic;
+import org.jeesl.interfaces.model.system.option.JeeslOptionRestDownload;
+import org.jeesl.interfaces.model.system.symbol.JeeslGraphic;
+
+import net.sf.ahtutils.interfaces.model.crud.EjbPersistable;
+import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
+import net.sf.ahtutils.interfaces.model.status.UtilsLang;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatusFixedCode;
+
+public interface JeeslQaTestStatus <S extends UtilsStatus<S,L,D>,L extends UtilsLang,D extends UtilsDescription,G extends JeeslGraphic<L,D,G,?,?,?>>
+									extends Serializable,EjbPersistable,UtilsStatusFixedCode,JeeslOptionRestDownload,EjbWithCodeGraphic<G>
+{	
+	public static enum Code{unread,discussion,accepted,rejected}
+}

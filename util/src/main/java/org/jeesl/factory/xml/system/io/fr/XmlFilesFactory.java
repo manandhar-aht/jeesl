734221108bc4f1e08367a3618e18ceb0673a1c97
Thorsten-K <t.kisner@web.de>
2018-03-19 10:37:59 +0100

    XmlFilesFactory added

---
+++ util/src/main/java/org/jeesl/factory/xml/system/io/fr/XmlFilesFactory.java
@@ -0,0 +1,35 @@
+package org.jeesl.factory.xml.system.io.fr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.jeesl.interfaces.model.system.io.fr.JeeslFileContainer;
+import org.jeesl.interfaces.model.system.io.fr.JeeslFileMeta;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.sf.exlp.factory.xml.io.XmlFileFactory;
+import net.sf.exlp.xml.io.Files;
+
+public class XmlFilesFactory<CONTAINER extends JeeslFileContainer<?,META>, META extends JeeslFileMeta<CONTAINER,?>>
+{
+	final static Logger logger = LoggerFactory.getLogger(XmlFilesFactory.class);
+	
+	public Files build(List<META> metas)
+	{
+		Files xml = net.sf.exlp.factory.xml.io.XmlFilesFactory.build();
+		
+		List<String> names = new ArrayList<String>();
+		for(META meta : metas)
+		{
+			xml.getFile().add(XmlFileFactory.build(meta.getName()));
+			names.add(meta.getName());
+		}
+		if(!names.isEmpty()) {xml.setName(StringUtils.join(names, ", "));}
+		xml.setElements(metas.size());
+		
+		
+		return xml;
+	}
+}

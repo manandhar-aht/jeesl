3026c5933ac293f2d0fc103d0c47d71d9377b62d
Petersen <petersen@aht-group.com>
2018-03-21 08:36:28 +0100

    FiguresXpath extended + Finance added counter

--- xml/src/main/java/net/sf/ahtutils/xml/finance/Finance.java
+++ xml/src/main/java/net/sf/ahtutils/xml/finance/Finance.java
@@ -65,7 +65,9 @@ public class Finance
     protected Double value;
     @XmlAttribute(name = "symbol")
     protected String symbol;
-
+    @XmlAttribute(name = "counter")
+    protected Double counter;
+    
     /**
      * Gets the value of the finance property.
      * 
@@ -310,5 +312,19 @@ public class Finance
     public boolean isSetSymbol() {
         return (this.symbol!= null);
     }
+
+	/**
+	 * @return the counter
+	 */
+	public Double getCounter() {
+		return counter;
+	}
+
+	/**
+	 * @param counter the counter to set
+	 */
+	public void setCounter(Double counter) {
+		this.counter = counter;
+	}
 
 }
--- xml/src/main/java/net/sf/ahtutils/xml/xpath/FiguresXpath.java
+++ xml/src/main/java/net/sf/ahtutils/xml/xpath/FiguresXpath.java
@@ -2,8 +2,13 @@ package net.sf.ahtutils.xml.xpath;
 
 import java.util.List;
 
+
+import net.sf.ahtutils.xml.finance.Counter;
 import net.sf.ahtutils.xml.finance.Figures;
 import net.sf.ahtutils.xml.finance.Finance;
+import net.sf.ahtutils.xml.finance.Time;
+import net.sf.ahtutils.xml.security.Staff;
+import net.sf.ahtutils.xml.security.Staffs;
 import net.sf.exlp.exception.ExlpXpathNotFoundException;
 import net.sf.exlp.exception.ExlpXpathNotUniqueException;
 
@@ -28,6 +33,67 @@ public class FiguresXpath
 		else if(list.size()>1){throw new ExlpXpathNotUniqueException("Multiple "+Finance.class.getSimpleName()+" for code="+code);}
 		return list.get(0);
 	}
+	
+	public static <E extends Enum<E>, C extends Enum<C>> Finance getFiguresFinance(Figures figures, E figureCode, C financeCode) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+	{
+		JXPathContext context = JXPathContext.newContext(figures);
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("figures[@code='").append(figureCode.toString()).append("']");
+		sb.append("/finance[@code='").append(financeCode).append("']");
+		
+		@SuppressWarnings("unchecked")
+		List<Finance> list = (List<Finance>)context.selectNodes(sb.toString());
+		if(list.size()==0){throw new ExlpXpathNotFoundException("No "+Finance.class.getSimpleName()+" for code="+figureCode+" - "+financeCode);}
+		else if(list.size()>1){throw new ExlpXpathNotUniqueException("Multiple "+Finance.class.getSimpleName()+" for code="+figureCode+" - "+financeCode);}
+		return list.get(0);
+	}
+	
+	public static <E extends Enum<E>, C extends Enum<C>> Finance getFiguresStaff(Figures figures, E figureCode, C staffCode) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+	{
+		JXPathContext context = JXPathContext.newContext(figures);
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("figures[@code='").append(figureCode.toString()).append("']");
+		sb.append("/finance[@code='").append(staffCode).append("']");
+		
+		@SuppressWarnings("unchecked")
+		List<Finance> list = (List<Finance>)context.selectNodes(sb.toString());
+		if(list.size()==0){throw new ExlpXpathNotFoundException("No "+Finance.class.getSimpleName()+" for code="+figureCode+" - "+staffCode);}
+		else if(list.size()>1){throw new ExlpXpathNotUniqueException("Multiple "+Finance.class.getSimpleName()+" for code="+figureCode+" - "+staffCode);}
+		return list.get(0);
+	}
+	
+	public static <E extends Enum<E>, C extends Enum<C>> Time getDatesTime(Figures figures, E figureCode, C datesCode) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+	{
+		JXPathContext context = JXPathContext.newContext(figures);
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("figures[@code='").append(figureCode.toString()).append("']");
+		sb.append("/time[@code='").append(datesCode).append("']");
+		
+		@SuppressWarnings("unchecked")
+		List<Time> list = (List<Time>)context.selectNodes(sb.toString());
+		if(list.size()==0){throw new ExlpXpathNotFoundException("No "+Finance.class.getSimpleName()+" for code="+figureCode+" - "+datesCode);}
+		else if(list.size()>1){throw new ExlpXpathNotUniqueException("Multiple "+Finance.class.getSimpleName()+" for code="+figureCode+" - "+datesCode);}
+		return list.get(0);
+	}
+	
+	public static <E extends Enum<E>, C extends Enum<C>> Counter getFiguresCounter(Figures figures, E figureCode, C counterCode) throws ExlpXpathNotFoundException, ExlpXpathNotUniqueException
+	{
+		JXPathContext context = JXPathContext.newContext(figures);
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("figures[@code='").append(figureCode.toString()).append("']");
+		sb.append("/counter[@code='").append(counterCode).append("']");
+		
+		@SuppressWarnings("unchecked")
+		List<Counter> list = (List<Counter>)context.selectNodes(sb.toString());
+		if(list.size()==0){throw new ExlpXpathNotFoundException("No "+Counter.class.getSimpleName()+" for code="+figureCode+" - "+counterCode);}
+		else if(list.size()>1){throw new ExlpXpathNotUniqueException("Multiple "+Counter.class.getSimpleName()+" for code="+figureCode+" - "+counterCode);}
+		return list.get(0);
+	}
+	
 	
 	public static Finance getFinance(List<Finance> list, int nr) throws ExlpXpathNotFoundException
 	{

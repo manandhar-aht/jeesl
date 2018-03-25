c3e8e9520b746b36cc3cb2ab0567c92caca9a618
Thorsten-K <t.kisner@web.de>
2018-03-25 20:31:24 +0200

    Mail Retention/Status added

---
+++ interfaces/src/main/java/org/jeesl/interfaces/model/system/io/mail/JeeslMailStatus.java
@@ -0,0 +1,19 @@
+package org.jeesl.interfaces.model.system.io.mail;
+
+import java.io.Serializable;
+
+import org.jeesl.interfaces.model.system.graphic.core.JeeslGraphic;
+import org.jeesl.interfaces.model.system.graphic.with.EjbWithCodeGraphic;
+import org.jeesl.interfaces.model.system.option.JeeslOptionRestDownload;
+
+import net.sf.ahtutils.interfaces.model.crud.EjbPersistable;
+import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
+import net.sf.ahtutils.interfaces.model.status.UtilsLang;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
+import net.sf.ahtutils.interfaces.model.status.UtilsStatusFixedCode;
+
+public interface JeeslMailStatus <S extends UtilsStatus<S,L,D>, L extends UtilsLang, D extends UtilsDescription,G extends JeeslGraphic<L,D,G,?,?,?>>
+					extends Serializable,EjbPersistable,JeeslOptionRestDownload,EjbWithCodeGraphic<G>,UtilsStatusFixedCode	
+{	
+	public enum Code{queue,spooling,sent,failed}
+}

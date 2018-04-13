54a78e4c88b2ae8df3aff151c298d2b06ed8f242
Petersen <petersen@aht-group.com>
2018-04-13 09:02:58 +0200

    added block to layout components

---
+++ jsf/src/main/java/net/sf/ahtutils/jsf/components/layout/Block.java
@@ -0,0 +1,86 @@
+package net.sf.ahtutils.jsf.components.layout;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIPanel;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+import net.sf.ahtutils.jsf.util.ComponentAttribute;
+import net.sf.ahtutils.model.interfaces.with.EjbWithId;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@FacesComponent("net.sf.ahtutils.jsf.components.layout.Block")
+public class Block extends UIPanel
+{
+	final static Logger logger = LoggerFactory.getLogger(Block.class);
+	private static enum Properties {id,width,styleClass,renderChildren,renderChildrenIfEjb,renderChildrenIfEjbPersisted}
+	
+	@Override public boolean getRendersChildren(){return true;}
+	
+	private Boolean renderChilds;
+	public Boolean getRenderChilds() {return renderChilds;}
+	public void setRenderChilds(Boolean renderChilds) {this.renderChilds = renderChilds;}
+
+	@Override
+	public void encodeBegin(FacesContext context) throws IOException
+	{
+		Map<String,Object> map = getAttributes();
+		
+		ResponseWriter responseWriter = context.getResponseWriter();
+		responseWriter.startElement("div", this);
+		responseWriter.writeAttribute("id",getClientId(context),"id");
+		
+		StringBuffer sbStyleClass = new StringBuffer();
+		sbStyleClass.append("auGrid_");
+		if(map.containsKey(Properties.width.toString()))
+		{
+			sbStyleClass.append(map.get(Properties.width.toString()));
+		}
+		else {sbStyleClass.append(12);}
+		if(map.containsKey(Properties.styleClass.toString()))
+		{
+			sbStyleClass.append(" ").append(map.get(Properties.styleClass.toString()));
+		}
+		responseWriter.writeAttribute("class",sbStyleClass.toString(),null);
+	}
+
+	@Override
+	public void encodeEnd(FacesContext context) throws IOException
+	{
+		ResponseWriter responseWriter = context.getResponseWriter();
+		responseWriter.endElement("div");
+	}
+	
+	@Override
+	public void encodeChildren(FacesContext context) throws IOException
+	{
+		boolean rChildren = ComponentAttribute.getBoolean(Properties.renderChildren.toString(), true, context, this);
+		
+		if(rChildren && ComponentAttribute.available(Properties.renderChildrenIfEjb.toString(),context,this))
+		{
+			EjbWithId ejb = ComponentAttribute.getObject(EjbWithId.class,Properties.renderChildrenIfEjb.toString(),context,this);
+			if(ejb==null){rChildren=false;}
+		}
+		
+		if(rChildren && ComponentAttribute.available(Properties.renderChildrenIfEjbPersisted.toString(),context,this))
+		{
+			EjbWithId ejb = ComponentAttribute.getObject(EjbWithId.class,Properties.renderChildrenIfEjbPersisted.toString(),context,this);
+			if(ejb==null){rChildren=false;}
+			else if(ejb.getId()==0){rChildren=false;}
+		}
+		
+		if(rChildren)
+		{
+			for(UIComponent uic : this.getChildren())
+			{
+				uic.encodeAll(context);
+			}
+		}
+	}
+}

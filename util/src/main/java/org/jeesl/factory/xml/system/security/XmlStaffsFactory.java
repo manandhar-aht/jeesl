c995096300c9834003d24a079f8309dbdcb69940
Thorsten-K <t.kisner@web.de>
2018-08-26 18:19:28 +0200

    Factories improved

--- util/src/main/java/org/jeesl/factory/xml/system/security/XmlStaffsFactory.java
+++ util/src/main/java/org/jeesl/factory/xml/system/security/XmlStaffsFactory.java
@@ -1,34 +1,21 @@
 package org.jeesl.factory.xml.system.security;
 
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityView;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityTemplate;
-import org.jeesl.interfaces.model.system.security.user.JeeslUser;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityAction;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityUsecase;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityCategory;
-import org.jeesl.interfaces.model.system.security.framework.JeeslSecurityRole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
-import net.sf.ahtutils.interfaces.model.status.UtilsLang;
+import net.sf.ahtutils.xml.security.Staff;
 import net.sf.ahtutils.xml.security.Staffs;
 
-public class XmlStaffsFactory <L extends UtilsLang,
-								D extends UtilsDescription, 
-								C extends JeeslSecurityCategory<L,D>,
-								R extends JeeslSecurityRole<L,D,C,V,U,A,USER>,
-								V extends JeeslSecurityView<L,D,C,R,U,A>,
-								U extends JeeslSecurityUsecase<L,D,C,R,V,A>,
-								A extends JeeslSecurityAction<L,D,R,V,U,AT>,
-								AT extends JeeslSecurityTemplate<L,D,C>,
-								USER extends JeeslUser<R>>
+public class XmlStaffsFactory
 {
 	final static Logger logger = LoggerFactory.getLogger(XmlStaffsFactory.class);
 			
-	public static Staffs build()
+	public static Staffs build() {return new Staffs();}
+	
+	public static Staffs build(Staff staff)
 	{
-		Staffs xml = new Staffs();
+		Staffs xml = build();
+		xml.getStaff().add(staff);
 		return xml;
 	}
 }

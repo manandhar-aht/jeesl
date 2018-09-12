bf4ecbe647f0ef727c67a5ec0553e5f0919a7963
Thorsten-K <t.kisner@web.de>
2018-09-12 10:17:25 +0200

    CodeFactory updated

--- util/src/main/java/org/jeesl/factory/ejb/util/EjbCodeFactory.java
+++ util/src/main/java/org/jeesl/factory/ejb/util/EjbCodeFactory.java
@@ -1,7 +1,7 @@
 package org.jeesl.factory.ejb.util;
 
+import java.util.Collection;
 import java.util.Hashtable;
-import java.util.List;
 import java.util.Map;
 
 import org.jeesl.interfaces.model.system.with.code.EjbWithCode;
@@ -11,8 +11,9 @@ import org.slf4j.LoggerFactory;
 public class EjbCodeFactory
 {
 	final static Logger logger = LoggerFactory.getLogger(EjbCodeFactory.class);
-	    
-    public static <T extends EjbWithCode> Map<String,T> toMapCode(List<T> list)
+	   
+	
+    public static <T extends EjbWithCode> Map<String,T> toMapCode(Collection<T> list)
     {
 	    	Map<String,T> map = new Hashtable<String,T>();
 	    	for(T s : list)

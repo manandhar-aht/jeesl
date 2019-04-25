eb08b512b2ed3d40d9ab58902f23deb75c6ea5f8
Thorsten-K <t.kisner@web.de>
2019-04-25 08:47:43 +0200

    TsDimension InputMethod updated

--- interfaces/src/main/java/org/jeesl/interfaces/model/module/ts/config/JeeslTsDimension.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/ts/config/JeeslTsDimension.java
@@ -12,15 +12,16 @@ import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatusFixedCode;
+import net.sf.ahtutils.interfaces.model.status.UtilsWithSymbol;
 
 public interface JeeslTsDimension <S extends UtilsStatus<S,L,D>,
 									L extends UtilsLang,
 									D extends UtilsDescription,
 									G extends JeeslGraphic<L,D,G,?,?,?>>
 					extends Serializable,EjbPersistable,
-								EjbWithCode,UtilsStatusFixedCode,
+								EjbWithCode,UtilsStatusFixedCode,UtilsWithSymbol,
 								JeeslOptionRestDownload,EjbWithCodeGraphic<G>,
 								UtilsStatus<S,L,D>
 {	
-	public enum Code{length,volume}
+	public enum Code{length,velocity,time,volume,temp}
 }
--- interfaces/src/main/java/org/jeesl/interfaces/model/module/ts/config/JeeslTsInputMethod.java
+++ interfaces/src/main/java/org/jeesl/interfaces/model/module/ts/config/JeeslTsInputMethod.java
@@ -3,8 +3,6 @@ package org.jeesl.interfaces.model.module.ts.config;
 import java.io.Serializable;
 
 import org.jeesl.interfaces.model.system.graphic.core.JeeslGraphic;
-import org.jeesl.interfaces.model.system.graphic.with.EjbWithCodeGraphic;
-import org.jeesl.interfaces.model.system.option.JeeslOptionRestDownload;
 import org.jeesl.interfaces.model.system.with.code.EjbWithCode;
 
 import net.sf.ahtutils.interfaces.model.crud.EjbPersistable;
@@ -16,7 +14,7 @@ import net.sf.ahtutils.interfaces.model.status.UtilsStatusFixedCode;
 public interface JeeslTsInputMethod <S extends UtilsStatus<S,L,D>, L extends UtilsLang, D extends UtilsDescription,G extends JeeslGraphic<L,D,G,?,?,?>>
 					extends Serializable,EjbPersistable,
 								EjbWithCode,UtilsStatusFixedCode,
-							//	JeeslOptionRestDownload,EjbWithCodeGraphic<G>,
+						//		JeeslOptionRestDownload,EjbWithCodeGraphic<G>,
 								UtilsStatus<S,L,D>
 {	
 	public enum Code{single,multi,manual}
